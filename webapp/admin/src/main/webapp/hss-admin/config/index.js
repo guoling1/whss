5ee991c4ec8d447d15ca546b775593c1668b30f3
于超 <chao.yu@jinkaimen.com>
2016-12-16 15:01:17 +0800

    修改admin路径

--- webapp/admin/src/main/webapp/hss-admin/config/index.js
+++ webapp/admin/src/main/webapp/hss-admin/config/index.js
@@ -6,8 +6,8 @@ module.exports = {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../../WEB-INF/index.html'),
     assetsRoot: path.resolve(__dirname, '../../'),
-    assetsSubDirectory: 'static.0.1.8',
-    assetsPublicPath: 'http://img.jinkaimen.cn/boss/hss/',
+    assetsSubDirectory: 'static.0.1.9',
+    assetsPublicPath: 'http://img.jinkaimen.cn/admin/hss/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
