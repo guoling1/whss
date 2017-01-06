6bde3b898cad4e501d419a4417fb39e87bea3823
guoling1 <646626114@qq.com>
2017-01-06 11:16:01 +0800

    代理还原

--- webapp/admin/src/main/webapp/hss-admin/src/components/AgentAdd.vue
+++ webapp/admin/src/main/webapp/hss-admin/src/components/AgentAdd.vue
@@ -2,192 +2,104 @@
   <div id="agentAdd">
     <div v-if="isShow" style="padding: 8px 30px; background: rgb(243, 156, 18); z-index: 999999; font-size: 22px; font-weight: 600;margin-bottom: 15px;color: #fff;">新增代理商</div>
     <div v-if="!isShow" style="padding: 8px 30px; background: rgb(243, 156, 18); z-index: 999999; font-size: 22px; font-weight: 600;margin-bottom: 15px;color: #fff;">代理商资料</div>
-    <div style="margin: 0px 15px 15px;">
-      <div class="box box-info">
+    <!--<h1 v-if="isShow">新增代理商</h1>
+    <h1 v-if="!isShow">代理商资料</h1>-->
+    <div style="margin: 0 10px">
+      <div class="box box-info" style="width: 50%">
         <div class="box-header with-border">
-          <h3 class="box-title">基本信息</h3>
-        </div>
-        <form class="form-horizontal">
-          <div class="box-body">
-            <div class="form-group">
-              <label for="mobile" class="col-sm-3 control-label">代理商手机号</label>
-
-              <div class="col-sm-4">
-                <input type="tel" id="mobile" class="form-control" name="mobile" placeholder="代理手机号，用于登录" v-model="$$data.mobile">
-              </div>
-            </div>
-            <div class="form-group">
-              <label for="name" class="col-sm-3 control-label">代理名称</label>
-
-              <div class="col-sm-4">
-                <input type="text" class="form-control" id="name" name="name" placeholder="" v-model="$$data.name">
-              </div>
-            </div>
-            <div class="form-group">
-              <label for="belongArea" class="col-sm-3 control-label">所在地</label>
-
-              <div class="col-sm-4">
-                <input type="text" class="form-control" id="belongArea" name="belongArea" placeholder="省/市/街道" v-model="$$data.belongArea">
-              </div>
-            </div>
-          </div>
-        </form>
-      </div>
-      <div class="box box-info">
-        <div class="box-header with-border">
-          <h3 class="box-title">结算信息</h3>
+          <h3 class="box-title" v-if="isShow">资料填写</h3>
+          <h3 class="box-title" v-if="!isShow">基本资料</h3>
         </div>
         <!-- /.box-header -->
         <!-- form start -->
         <form class="form-horizontal">
           <div class="box-body">
             <div class="form-group">
-              <label for="bankCard" class="col-sm-3 control-label">结算卡</label>
+              <label for="mobile" class="col-sm-2 control-label">代理手机</label>
 
-              <div class="col-sm-4">
+              <div class="col-sm-9">
+                <input type="tel" id="mobile" class="form-control" name="mobile" placeholder="代理手机号，用于登录" v-model="$$data.mobile">
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="name" class="col-sm-2 control-label">代理名称</label>
+
+              <div class="col-sm-9">
+                <input type="text" class="form-control" id="name" name="name" placeholder="输入商户名称" v-model="$$data.name">
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="belongArea" class="col-sm-2 control-label">所在地</label>
+
+              <div class="col-sm-9">
+                <input type="text" class="form-control" id="belongArea" name="belongArea" placeholder="省/市/街道" v-model="$$data.belongArea">
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="bankCard" class="col-sm-2 control-label">结算卡</label>
+
+              <div class="col-sm-9">
                 <input type="text" class="form-control" id="bankCard" name="bankCard" placeholder="输入一级代理银行卡号" v-model="$$data.bankCard">
               </div>
             </div>
             <div class="form-group">
-              <label for="bankAccountName" class="col-sm-3 control-label">开户名</label>
+              <label for="bankAccountName" class="col-sm-2 control-label">开户名</label>
 
-              <div class="col-sm-4">
+              <div class="col-sm-9">
                 <input type="text" class="form-control" id="bankAccountName" name="bankAccountName" placeholder="银行开户名称" v-model="$$data.bankAccountName">
               </div>
             </div>
             <div class="form-group">
-              <label for="idCard" class="col-sm-3 control-label">身份证号</label>
+              <label for="bankReserveMobile" class="col-sm-2 control-label">手机号</label>
 
-              <div class="col-sm-4">
-                <input type="text" class="form-control" id="idCard" name="idCard" placeholder="" v-model="$$data.idCard">
-              </div>
-            </div>
-            <div class="form-group">
-              <label for="bankReserveMobile" class="col-sm-3 control-label">开户手机号</label>
-
-              <div class="col-sm-4">
+              <div class="col-sm-9">
                 <input type="text" class="form-control" id="bankReserveMobile" name="bankReserveMobile" placeholder="银行开户预留手机号" v-model="$$data.bankReserveMobile">
               </div>
             </div>
           </div>
           <!-- /.box-body -->
-        </form>
-      </div>
-      <div class="box box-info">
-        <div class="box-header with-border">
-          <h3 class="box-title">产品分润设置</h3>
-        </div>
-        <form class="form-horizontal">
-          <div class="box-body">
-            <div class="form-group">
-              <div class="product">
-                <label v-for="(product,index) in $$products">
-                  <input class="check" type="radio" name="name" :value="index" v-model="id">
 
-                  <div class="product1">
-                    <div class="col-xs-12">
-                      <div class="box box1">
-                        <div class="box-header">
-                          <h3 class="box-title">{{product.productName}}</h3>
-                        </div>
-                        <!-- /.box-header -->
-                        <div class="box-body table-responsive no-padding">
-                          <table class="table table-hover">
-                            <tbody>
-                            <tr>
-                              <th>通道名称</th>
-                              <th>支付结算手续费</th>
-                              <th>结算时间</th>
-                              <th>提现结算费</th>
-                              <th>商户支付手续费</th>
-                              <th>商户提现手续费</th>
-                            </tr>
-                            <tr v-for="channel in product.list">
-                              <td>{{channel.channelTypeSign|changeName}}</td>
-                              <td><input type="text" name="name" v-model="channel.paymentSettleRate">%</td>
-                              <td>{{channel.productBalanceType}}</td>
-                              <td><input type="text" name="name" v-model="channel.withdrawSettleFee">元/笔</td>
-                              <td><input type="text" name="name" v-model="channel.merchantSettleRate">%</td>
-                              <td><input type="text" name="name" v-model="channel.merchantWithdrawFee">元/笔</td>
-                            </tr>
-                            </tbody></table>
-                        </div>
-                        <!-- /.box-body -->
-                      </div>
-                      <!-- /.box -->
-                    </div>
-                  </div>
-                </label>
-              </div>
-            </div>
-          </div>
         </form>
       </div>
-      <div class="box box-info">
-        <div class="box-header with-border">
-          <h3 class="box-title">合伙人推荐功能开关</h3>
-        </div>
-        <form class="form-horizontal">
-          <div class="box-body">
-            <div class="form-group">
-              <select class="form-control select2 select2-hidden-accessible" style="width: 25%;margin-left: 10%;" tabindex="-1" aria-hidden="true" v-model="recommendBtn">
-                <option value="2">开</option>
-                <option value="1">关</option>
-              </select>
-            </div>
-          </div>
-          <!-- /.box-body -->
-        </form>
-      </div>
-      <div class="box box-info">
-        <div class="box-header with-border">
-          <h3 class="box-title">合伙人推荐分润设置</h3>
-        </div>
-        <form class="form-horizontal">
-          <div class="box-body">
-            <div class="form-group">
-              <div class="product">
-                <div class="col-xs-12">
-                      <div class="box box1">
-                        <div class="box-body table-responsive no-padding">
-                          <table class="table table-hover">
-                            <tbody>
-                            <tr>
-                              <th>收单总分润空间</th>
-                              <th colspan="5">
-                                <input type="number" style="width: 20%" v-model="totalProfitSpace">%
-                                （总分润空间不可高于0.2%，收单分润需扣除商户升级及推荐的分润成本）
-                              </th>
-                            </tr>
-                            <tr>
-                              <td>分润类型</td>
-                              <td>金开门分润比例</td>
-                              <td>一级代理商分润比例</td>
-                              <td>二级代理分润比例</td>
-                              <td rowspan="3" style="width: 20%">金开门，一级代理，二级代理的比例之和必须等于100%</td>
-                            </tr>
-                            <tr>
-                              <td>升级费分润</td>
-                              <td>20%</td>
-                              <td><input type="number" v-model="rate1">%</td>
-                              <td><input type="number" v-model="rate2">%</td>
-                            </tr>
-                            <tr>
-                              <td>收单分润</td>
-                              <td>20%</td>
-                              <td><input type="number" v-model="rate3">%</td>
-                              <td><input type="number" v-model="rate4">%</td>
-                            </tr>
-                            </tbody></table>
-                        </div>
-                        <!-- /.box-body -->
-                      </div>
-                      <!-- /.box -->
-                    </div>
+      <div class="product">
+        <p>配置产品：</p>
+        <label v-for="(product,index) in $$products">
+          <input class="check" type="radio" name="name" :value="index" v-model="id">
+
+          <div class="product1">
+            <div class="col-xs-12">
+              <div class="box">
+                <div class="box-header">
+                  <h3 class="box-title">{{product.productName}}</h3>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body table-responsive no-padding">
+                  <table class="table table-hover">
+                    <tbody>
+                    <tr>
+                      <th>通道名称</th>
+                      <th>支付结算手续费</th>
+                      <th>结算时间</th>
+                      <th>提现结算费</th>
+                      <th>商户支付手续费</th>
+                      <th>商户提现手续费</th>
+                    </tr>
+                    <tr v-for="channel in product.list">
+                      <td>{{channel.channelTypeSign|changeName}}</td>
+                      <td><input type="text" name="name" v-model="channel.paymentSettleRate">%</td>
+                      <td>{{channel.productBalanceType}}</td>
+                      <td><input type="text" name="name" v-model="channel.withdrawSettleFee">元/笔</td>
+                      <td><input type="text" name="name" v-model="channel.merchantSettleRate">%</td>
+                      <td><input type="text" name="name" v-model="channel.merchantWithdrawFee">元/笔</td>
+                    </tr>
+                    </tbody></table>
+                </div>
+                <!-- /.box-body -->
               </div>
+              <!-- /.box -->
             </div>
           </div>
-        </form>
+        </label>
       </div>
       <div class="btn btn-default" @click="create" v-if="isShow">
         创建代理商
@@ -214,35 +126,12 @@
         bankCard: '',
         bankAccountName: '',
         bankReserveMobile: '',
-        idCard:'',
         product: {
           channels: []
         },
         products: [],//所有产品
         id: 0,
-        isShow:true,
-        recommendBtn:2,
-        totalProfitSpace:'',
-        rate1:'',
-        rate2:'',
-        rate3:'',
-        rate4:'',
-        dealerUpgerdeRates:[
-          {
-            productId:'',
-            type:1,
-            firstDealerShareProfitRate:'',
-            secondDealerShareProfitRate:'',
-            bossDealerShareRate:0.2
-          },
-          {
-            productId:'',
-            type:2,
-            firstDealerShareProfitRate:'',
-            secondDealerShareProfitRate:'',
-            bossDealerShareRate:0.2
-          }
-        ],
+        isShow:true
       }
     },
     created: function () {
@@ -266,20 +155,15 @@
                   this.$data.belongArea = res.data.belongArea;
                   this.$data.bankCard = res.data.bankCard;
                   this.$data.bankAccountName = res.data.bankAccountName;
-                  this.$data.idCard = res.data.idCard;
-                  this.$data.recommendBtn = res.data.recommendBtn;
-                  this.$data.totalProfitSpace = res.data.totalProfitSpace*100;
                   this.$data.bankReserveMobile = res.data.bankReserveMobile;
                   this.$data.products = [res.data.product];
                   this.$data.products[0].list = res.data.product.channels
                   for(var i= 0; i < this.$data.products[0].list.length; i++){
                       this.$data.products[0].list[i].channelTypeSign = this.$data.products[0].list[i].channelType
                       this.$data.products[0].list[i].productBalanceType = this.$data.products[0].list[i].settleType
+
                   }
-                  this.$data.rate1 = res.data.dealerUpgerdeRates[0].firstDealerShareProfitRate*100;
-                  this.$data.rate2 = res.data.dealerUpgerdeRates[0].secondDealerShareProfitRate*100;
-                  this.$data.rate3 = res.data.dealerUpgerdeRates[1].firstDealerShareProfitRate*100;
-                  this.$data.rate4 = res.data.dealerUpgerdeRates[1].secondDealerShareProfitRate*100;
+                  console.log(this.$data.products[0])
                 })
             }
           }, function (err) {
@@ -287,13 +171,13 @@
               text: err.statusMessage
             })
           })
+
+
     },
     methods: {
       create: function () {
         this.$data.product.channels = [];
         this.$data.product.productId = this.$data.products[this.$data.id].productId;
-        this.$data.dealerUpgerdeRates[0].productId = this.$data.products[this.$data.id].productId;
-        this.$data.dealerUpgerdeRates[1].productId = this.$data.products[this.$data.id].productId;
         for (let i = 0; i < this.$data.products[this.$data.id].list.length; i++) {
           this.$data.product.channels.push({
             channelType: this.$data.products[this.$data.id].list[i].channelTypeSign,
@@ -304,11 +188,6 @@
             merchantWithdrawFee: this.$data.products[this.$data.id].list[i].merchantWithdrawFee
           })
         }
-        this.$data.totalProfitSpace = this.$data.totalProfitSpace/100;
-        this.$data.dealerUpgerdeRates[0].firstDealerShareProfitRate= this.$data.rate1/100;
-        this.$data.dealerUpgerdeRates[0].secondDealerShareProfitRate= this.$data.rate2/100;
-        this.$data.dealerUpgerdeRates[1].firstDealerShareProfitRate= this.$data.rate3/100;
-        this.$data.dealerUpgerdeRates[1].secondDealerShareProfitRate= this.$data.rate4/100;
         let query = {
           mobile: this.$data.mobile,
           name: this.$data.name,
@@ -316,11 +195,7 @@
           bankCard: this.$data.bankCard,
           bankAccountName: this.$data.bankAccountName,
           bankReserveMobile: this.$data.bankReserveMobile,
-          product: this.$data.product,
-          idCard: this.$data.idCard,
-          recommendBtn: this.$data.recommendBtn,
-          totalProfitSpace: this.$data.totalProfitSpace,
-          dealerUpgerdeRates: this.$data.dealerUpgerdeRates
+          product: this.$data.product
         };
         this.$http.post('/admin/user/addFirstDealer', query)
           .then(function (res) {
@@ -337,12 +212,9 @@
       goBack: function () {
         this.$router.push('/admin/record/agentList')
       },
-      //修改
       change: function () {
         this.$data.product.channels = [];
         this.$data.product.productId = this.$data.products[this.$data.id].productId;
-        this.$data.dealerUpgerdeRates[0].productId = this.$data.products[this.$data.id].productId;
-        this.$data.dealerUpgerdeRates[1].productId = this.$data.products[this.$data.id].productId;
         for (let i = 0; i < this.$data.products[this.$data.id].list.length; i++) {
           this.$data.product.channels.push({
             channelType: this.$data.products[this.$data.id].list[i].channelTypeSign,
@@ -353,11 +225,6 @@
             merchantWithdrawFee: this.$data.products[this.$data.id].list[i].merchantWithdrawFee
           })
         }
-        this.$data.totalProfitSpace = this.$data.totalProfitSpace/100;
-        this.$data.dealerUpgerdeRates[0].firstDealerShareProfitRate= this.$data.rate1/100;
-        this.$data.dealerUpgerdeRates[0].secondDealerShareProfitRate= this.$data.rate2/100;
-        this.$data.dealerUpgerdeRates[1].firstDealerShareProfitRate= this.$data.rate3/100;
-        this.$data.dealerUpgerdeRates[1].secondDealerShareProfitRate= this.$data.rate4/100;
         let query = {
           dealerId: this.$route.query.id,
           mobile: this.$data.mobile,
@@ -366,11 +233,7 @@
           bankCard: this.$data.bankCard,
           bankAccountName: this.$data.bankAccountName,
           bankReserveMobile: this.$data.bankReserveMobile,
-          product: this.$data.product,
-          idCard: this.$data.idCard,
-          recommendBtn: this.$data.recommendBtn,
-          totalProfitSpace: this.$data.totalProfitSpace,
-          dealerUpgerdeRates: this.$data.dealerUpgerdeRates
+          product: this.$data.product
         };
         this.$http.post('/admin/user/updateDealer', query)
           .then(function (res) {
@@ -391,7 +254,7 @@
       },
       $$data: function () {
         return this.$data;
-      },
+      }
     },
     filters: {
       changeName: function (val) {
@@ -431,28 +294,32 @@
   a {
     color: #42b983;
   }
-  .box1{
-    border-top: 1px solid #d2d6de;
-    box-shadow: 0 1px 1px rgba(0,0,0,0.4);
-  }
-  tr th,tr td{
-    text-align: center;
+
+  .agentAdd {
+    float: right;
+    width: 80%;
   }
 
-  .middle{
-    position: relative;
-    i{
-      position: absolute;
-      top:7px;
-      right:0;
+  .add {
+    border: 1px solid #ccc;
+    width: 800px;
+    border-top: none;
+    font-size: 16px;
+
+    th {
+      width: 200px;
+      vertical-align: middle;
+      text-align: center;
+    }
+    td input {
+      border: none;
+      width: 600px;
     }
   }
-  .right{
-    padding-top: 7px;
-  }
+
   .product1 {
     display: inline-block;
-    width: 95%;
+    width: 90%;
   }
 
   input.check {
@@ -460,14 +327,12 @@
     top: -50px;
   }
   .product label{
-    width: 100%;
-    margin-left: 2%;
+    width: 90%;
   }
   .product .table {
     input {
       width: 77%;
       border: none;
-      border-bottom: 1px solid #d0d0d0;
     }
   }
 </style>
