68970201e8877c8fae66336e99ddcbcbd2e5a0ae
zhangyulong <yulong.zhang@jinkaimen.com>
2017-01-12 14:03:41 +0800

    shenhe

---
+++ service/settle/settle-api/src/main/java/com/jkm/hss/settle/entity/AccountSettleAuditRecord.java
@@ -0,0 +1,14 @@
+package com.jkm.hss.settle.entity;
+
+import lombok.Data;
+
+/**
+ * Created by yulong.zhang on 2017/1/12.
+ *
+ * 结算审核记录
+ *
+ * tb
+ */
+@Data
+public class AccountSettleAuditRecord {
+}
