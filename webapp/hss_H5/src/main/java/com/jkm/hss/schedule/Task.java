89cf100658ab471a24dbf1082de14af7da004d68
zhangyulong <yulong.zhang@jinkaimen.com>
2017-01-12 20:47:51 +0800

    appId

---
+++ webapp/hss_H5/src/main/java/com/jkm/hss/schedule/Task.java
@@ -0,0 +1,17 @@
+package com.jkm.hss.schedule;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by yulong.zhang on 2017/1/12.
+ */
+
+@Slf4j
+@Component
+@Lazy(false)
+public class Task {
+
+
+}
