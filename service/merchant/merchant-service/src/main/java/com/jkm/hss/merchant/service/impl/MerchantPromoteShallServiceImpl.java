36ca27205e23bbac3a5732d73f898b3515cadb5e
xiang.yu <http://gitlab.jinkaimen.com>
2017-01-12 21:35:24 +0800

    升级费分润， 提现分润

--- service/merchant/merchant-service/src/main/java/com/jkm/hss/merchant/service/impl/MerchantPromoteShallServiceImpl.java
+++ service/merchant/merchant-service/src/main/java/com/jkm/hss/merchant/service/impl/MerchantPromoteShallServiceImpl.java
@@ -94,7 +94,7 @@ public class MerchantPromoteShallServiceImpl implements MerchantPromoteShallServ
                 BigDecimal inDirectMoney = null;
                 MerchantInfo inDirectMerchantInfo = null;
                 //上上级商户分润，间推分润
-                if (merchantInfo.getSecondDealerId() != 0){
+                if (merchantInfo.getSecondMerchantId() != 0){
                     inDirectMerchantInfo = this.merchantInfoService.selectById(merchantInfo.getSecondMerchantId()).get();
                     inDirectMoney = pair.getRight();
                 }else{
