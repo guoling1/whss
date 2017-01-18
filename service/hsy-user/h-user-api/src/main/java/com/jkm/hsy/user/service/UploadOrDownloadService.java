61e3bb0936781b59e642f87fa1c0c80805b1633c
Administrator <706326182@qq.com>
2017-01-18 14:03:26 +0800

    分润优化

--- service/hsy-user/h-user-api/src/main/java/com/jkm/hsy/user/service/UploadOrDownloadService.java
+++ service/hsy-user/h-user-api/src/main/java/com/jkm/hsy/user/service/UploadOrDownloadService.java
@@ -1,13 +1,14 @@
 package com.jkm.hsy.user.service;
 
-import org.omg.CORBA.portable.InputStream;
+import java.io.InputStream;
 
 /**
  * Created by Administrator on 2017/1/17.
  */
 public interface UploadOrDownloadService {
 
-    String getHsyUrl(InputStream inputStream, String fileName);
+
+    void getHsyUrl(InputStream inputStream, String photoName);
 
 
 }
