a1e10bb75ba6dd22e64fb1e67601e4d717bb1511
xingliujie <java_xlj@163.com>
2017-01-18 14:59:09 +0800

    初始化

--- webapp/hsy_API/src/main/java/com/jkm/hss/controller/code/CodeController.java
+++ webapp/hsy_API/src/main/java/com/jkm/hss/controller/code/CodeController.java
@@ -4,6 +4,7 @@ import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
 import com.jkm.base.common.entity.CommonResponse;
 import com.jkm.hss.admin.entity.QRCode;
+import com.jkm.hss.admin.enums.EnumQRCodeSysType;
 import com.jkm.hss.admin.service.QRCodeService;
 import com.jkm.hss.controller.BaseController;
 import com.jkm.hss.helper.ApplicationConsts;
@@ -13,6 +14,7 @@ import com.jkm.hss.merchant.enums.EnumMerchantStatus;
 import com.jkm.hss.merchant.helper.WxConstants;
 import com.jkm.hss.merchant.service.MerchantInfoService;
 import com.jkm.hss.merchant.service.UserInfoService;
+import com.jkm.hsy.user.dao.HsyShopDao;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -28,7 +30,7 @@ import java.net.URLDecoder;
 import java.net.URLEncoder;
 
 /**
- * Created by yulong.zhang on 2016/11/29.
+ * 微信扫码入口
  */
 @Slf4j
 @Controller
@@ -40,109 +42,158 @@ public class CodeController extends BaseController {
 
     @Autowired
     private MerchantInfoService merchantInfoService;
+
     @Autowired
-    private UserInfoService userInfoService;
+    private HsyShopDao hsyShopDao;
 
     /**
      * 扫码
-     *
      * @param request
      * @param response
      * @return
      */
     @RequestMapping(value = "/scanCode", method = RequestMethod.GET)
     public String scanCode(final HttpServletRequest request, final HttpServletResponse response, final Model model,@RequestParam(value = "openId", required = false) String openId) {
-        //如何没有openId跳授权页面
-        if(openId==null||"".equals(openId)){
-            String requestUrl = "";
-            if(request.getQueryString() == null){
-                requestUrl = "";
-            }else{
-                requestUrl = request.getQueryString();
-            }
-            try {
-                String encoderUrl = URLEncoder.encode(requestUrl, "UTF-8");
-                return "redirect:"+ WxConstants.WEIXIN_MERCHANT_USERINFO+encoderUrl+ WxConstants.WEIXIN_USERINFO_REDIRECT;
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
-
-        }
+        boolean isRedirect = true;
         final String code = request.getParameter("code");
         final String sign = request.getParameter("sign");
-        if ((Long.valueOf(code) >= Long.valueOf("100010063208")) && (Long.valueOf(code) <= Long.valueOf("100010068207"))) {
-            return "redirect:http://"+ ApplicationConsts.getApplicationConfig().domain()+"/code/scanCode?" + "code" + "=" + code + "&" + "sign" + "=" + sign;
-        }
         log.info("scan code[{}], sign is [{}]", code, sign);
         final Optional<QRCode> qrCodeOptional = this.qrCodeService.getByCode(code);
-        Preconditions.checkState(qrCodeOptional.isPresent(), "QRCode not exist");
+        Preconditions.checkState(qrCodeOptional.isPresent(), "二维码不存在");
+        Preconditions.checkState((qrCodeOptional.get().getSysType()).equals(EnumQRCodeSysType.HSY.getId()), "二维码不属于该系统");
         final QRCode qrCode = qrCodeOptional.get();
-        //未分配，也可以扫码
-//        Preconditions.checkState(qrCode.isDistribute(), "QRCode not distribute");
         Preconditions.checkState(qrCode.isCorrectSign(sign), "sign is not correct");
         final long merchantId = qrCode.getMerchantId();
         final String agent = request.getHeader("User-Agent");
         String url = "";
         if (qrCode.isActivate()) {//已激活
-            Optional<UserInfo> userInfoOptional = userInfoService.selectByMerchantId(merchantId);
-            Preconditions.checkState(userInfoOptional.isPresent(), "userInfo is not exist");
-            String openIdTemp = userInfoOptional.get().getOpenId();
-            if(openId.equals(openIdTemp)){
-                log.info("code[{}] is activate", code);
-                final Optional<MerchantInfo> merchantInfoOptional = this.merchantInfoService.selectById(merchantId);
-                Preconditions.checkState(merchantInfoOptional.isPresent(), "merchant is not exist");
-                final MerchantInfo merchantInfo = merchantInfoOptional.get();
-                model.addAttribute("merchantId", merchantId);
-                if (EnumMerchantStatus.INIT.getId() == merchantInfo.getStatus()) { // 初始化-->填资料
-                    log.info("code[{}], merchant is in init; go to fill profile", code);
-                    url = "/sqb/addInfo";
-                } else if (EnumMerchantStatus.ONESTEP.getId() == merchantInfo.getStatus()) { // 初始化-->图片
-                    log.info("code[{}], merchant is in one step; go to fill pic", code);
-                    url = "/sqb/addNext";
-                } else if (EnumMerchantStatus.REVIEW.getId() == merchantInfo.getStatus()) {//待审核
-                    log.info("code[{}], merchant is in due audit", code);
-                    url = "/sqb/prompt";
-                } else if (EnumMerchantStatus.UNPASSED.getId() == merchantInfo.getStatus()) {//审核未通过
-                    log.info("code[{}], merchant is not pass", code);
-                    url = "/sqb/prompt";
-                } else if (EnumMerchantStatus.PASSED.getId() == merchantInfo.getStatus()) {//审核通过
-                    model.addAttribute("name", merchantInfo.getMerchantName());
-                    if (agent.indexOf("MicroMessenger") > -1) {//weixin
-                        url = "/sqb/paymentWx";
-                    }
-                    if (agent.indexOf("AliApp") > -1) {// AliApp
-                        url = "/sqb/paymentZfb";
-                    }
-                }else if (EnumMerchantStatus.LOGIN.getId() == merchantInfo.getStatus()) {//注册
-                    model.addAttribute("code", code);
-                    url =  "/sqb/reg";
-                } else {
-                    log.error("code[{}] is activate, but merchant[{}] is disabled", code, merchantId);
-                }
-            }else{
-                log.info("code[{}] is activate", code);
-                final Optional<MerchantInfo> merchantInfoOptional = this.merchantInfoService.selectById(merchantId);
-                Preconditions.checkState(merchantInfoOptional.isPresent(), "merchant is not exist");
-                final MerchantInfo merchantInfo = merchantInfoOptional.get();
-                model.addAttribute("merchantId", merchantId);
-                if (EnumMerchantStatus.PASSED.getId() == merchantInfo.getStatus()) {//审核通过
-                    model.addAttribute("name", merchantInfo.getMerchantName());
-                    if (agent.indexOf("MicroMessenger") > -1) {//weixin
-                        url = "/sqb/paymentWx";
-                    }
-                    if (agent.indexOf("AliApp") > -1) {// AliApp
-                        url = "/sqb/paymentZfb";
-                    }
-                }else{
-                    log.info("code[{}], merchant is not pass", code);
-                    url = "/sqb/unFinishedPrompt";
-                }
+            log.info("code[{}] is activate", code);
+            final Optional<MerchantInfo> merchantInfoOptional = this.merchantInfoService.selectById(merchantId);
+            Preconditions.checkState(merchantInfoOptional.isPresent(), "商户不存在");
+            final MerchantInfo merchantInfo = merchantInfoOptional.get();
+            model.addAttribute("merchantId", merchantId);
+            model.addAttribute("name", merchantInfo.getMerchantName());
+            log.info("设备标示{}",agent.indexOf("MicroMessenger"));
+            if (agent.indexOf("MicroMessenger") > -1) {
+                url = "/sqb/paymentWx";
             }
-        } else {//注册
-            Preconditions.checkState(agent.indexOf("MicroMessenger") > -1, "please register in weixin");
-            model.addAttribute("code", code);
-            url =  "/sqb/reg";
+            if (agent.indexOf("AliApp") > -1) {
+                url = "/sqb/paymentZfb";
+            }
+        } else {
+            log.info("code[{}] is not activate", code);
+            isRedirect = false;
+            model.addAttribute("message", "该二维码未激活");
+            url = "/message";
         }
-        return "redirect:"+url;
+        if(isRedirect){
+            return "redirect:"+url;
+        }else{
+            return url;
+        }
     }
+
+
+//    /**
+//     * 扫码
+//     *
+//     * @param request
+//     * @param response
+//     * @return
+//     */
+//    @RequestMapping(value = "/scanCode", method = RequestMethod.GET)
+//    public String scanCode(final HttpServletRequest request, final HttpServletResponse response, final Model model,@RequestParam(value = "openId", required = false) String openId) {
+//        //如何没有openId跳授权页面
+//        if(openId==null||"".equals(openId)){
+//            String requestUrl = "";
+//            if(request.getQueryString() == null){
+//                requestUrl = "";
+//            }else{
+//                requestUrl = request.getQueryString();
+//            }
+//            try {
+//                String encoderUrl = URLEncoder.encode(requestUrl, "UTF-8");
+//                return "redirect:"+ WxConstants.WEIXIN_MERCHANT_USERINFO+encoderUrl+ WxConstants.WEIXIN_USERINFO_REDIRECT;
+//            } catch (UnsupportedEncodingException e) {
+//                e.printStackTrace();
+//            }
+//
+//        }
+//        final String code = request.getParameter("code");
+//        final String sign = request.getParameter("sign");
+//        if ((Long.valueOf(code) >= Long.valueOf("100010063208")) && (Long.valueOf(code) <= Long.valueOf("100010068207"))) {
+//            return "redirect:http://"+ ApplicationConsts.getApplicationConfig().domain()+"/code/scanCode?" + "code" + "=" + code + "&" + "sign" + "=" + sign;
+//        }
+//        log.info("scan code[{}], sign is [{}]", code, sign);
+//        final Optional<QRCode> qrCodeOptional = this.qrCodeService.getByCode(code);
+//        Preconditions.checkState(qrCodeOptional.isPresent(), "QRCode not exist");
+//        final QRCode qrCode = qrCodeOptional.get();
+//        //未分配，也可以扫码
+////        Preconditions.checkState(qrCode.isDistribute(), "QRCode not distribute");
+//        Preconditions.checkState(qrCode.isCorrectSign(sign), "sign is not correct");
+//        final long merchantId = qrCode.getMerchantId();
+//        final String agent = request.getHeader("User-Agent");
+//        String url = "";
+//        if (qrCode.isActivate()) {//已激活
+//            Optional<UserInfo> userInfoOptional = userInfoService.selectByMerchantId(merchantId);
+//            Preconditions.checkState(userInfoOptional.isPresent(), "userInfo is not exist");
+//            String openIdTemp = userInfoOptional.get().getOpenId();
+//            if(openId.equals(openIdTemp)){
+//                log.info("code[{}] is activate", code);
+//                final Optional<MerchantInfo> merchantInfoOptional = this.merchantInfoService.selectById(merchantId);
+//                Preconditions.checkState(merchantInfoOptional.isPresent(), "merchant is not exist");
+//                final MerchantInfo merchantInfo = merchantInfoOptional.get();
+//                model.addAttribute("merchantId", merchantId);
+//                if (EnumMerchantStatus.INIT.getId() == merchantInfo.getStatus()) { // 初始化-->填资料
+//                    log.info("code[{}], merchant is in init; go to fill profile", code);
+//                    url = "/sqb/addInfo";
+//                } else if (EnumMerchantStatus.ONESTEP.getId() == merchantInfo.getStatus()) { // 初始化-->图片
+//                    log.info("code[{}], merchant is in one step; go to fill pic", code);
+//                    url = "/sqb/addNext";
+//                } else if (EnumMerchantStatus.REVIEW.getId() == merchantInfo.getStatus()) {//待审核
+//                    log.info("code[{}], merchant is in due audit", code);
+//                    url = "/sqb/prompt";
+//                } else if (EnumMerchantStatus.UNPASSED.getId() == merchantInfo.getStatus()) {//审核未通过
+//                    log.info("code[{}], merchant is not pass", code);
+//                    url = "/sqb/prompt";
+//                } else if (EnumMerchantStatus.PASSED.getId() == merchantInfo.getStatus()) {//审核通过
+//                    model.addAttribute("name", merchantInfo.getMerchantName());
+//                    if (agent.indexOf("MicroMessenger") > -1) {//weixin
+//                        url = "/sqb/paymentWx";
+//                    }
+//                    if (agent.indexOf("AliApp") > -1) {// AliApp
+//                        url = "/sqb/paymentZfb";
+//                    }
+//                }else if (EnumMerchantStatus.LOGIN.getId() == merchantInfo.getStatus()) {//注册
+//                    model.addAttribute("code", code);
+//                    url =  "/sqb/reg";
+//                } else {
+//                    log.error("code[{}] is activate, but merchant[{}] is disabled", code, merchantId);
+//                }
+//            }else{
+//                log.info("code[{}] is activate", code);
+//                final Optional<MerchantInfo> merchantInfoOptional = this.merchantInfoService.selectById(merchantId);
+//                Preconditions.checkState(merchantInfoOptional.isPresent(), "merchant is not exist");
+//                final MerchantInfo merchantInfo = merchantInfoOptional.get();
+//                model.addAttribute("merchantId", merchantId);
+//                if (EnumMerchantStatus.PASSED.getId() == merchantInfo.getStatus()) {//审核通过
+//                    model.addAttribute("name", merchantInfo.getMerchantName());
+//                    if (agent.indexOf("MicroMessenger") > -1) {//weixin
+//                        url = "/sqb/paymentWx";
+//                    }
+//                    if (agent.indexOf("AliApp") > -1) {// AliApp
+//                        url = "/sqb/paymentZfb";
+//                    }
+//                }else{
+//                    log.info("code[{}], merchant is not pass", code);
+//                    url = "/sqb/unFinishedPrompt";
+//                }
+//            }
+//        } else {//注册
+//            Preconditions.checkState(agent.indexOf("MicroMessenger") > -1, "please register in weixin");
+//            model.addAttribute("code", code);
+//            url =  "/sqb/reg";
+//        }
+//        return "redirect:"+url;
+//    }
 }
---
+++ webapp/hsy_API/src/main/java/com/jkm/hss/controller/code/SkipController.java
@@ -0,0 +1,71 @@
+package com.jkm.hss.controller.code;
+
+import com.jkm.hss.controller.BaseController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Thinkpad on 2017/1/18.
+ */
+@Slf4j
+@Controller
+@RequestMapping(value = "/sqb")
+public class SkipController extends BaseController {
+    /**
+     * 支付升级成功页面
+     * @param request
+     * @param response
+     * @param model
+     * @return
+     * @throws IOException
+     */
+    @RequestMapping(value = "/success/{amount}/{orderId}", method = RequestMethod.GET)
+    public String buySuccess(final HttpServletRequest request, final HttpServletResponse response, final Model model, @PathVariable("amount") String amount, @PathVariable("orderId") String orderId) throws IOException {
+        model.addAttribute("money", amount);
+        model.addAttribute("firstSn", orderId.substring(0, orderId.length() - 6));
+        model.addAttribute("secondSn", orderId.substring(orderId.length() - 6, orderId.length()));
+        return "/buySuccess";
+    }
+    /**
+     * 扫固定码微信支付页面
+     * @param request
+     * @param response
+     * @param model
+     * @param merchantId
+     * @param name
+     * @return
+     * @throws IOException
+     */
+    @RequestMapping(value = "/paymentWx", method = RequestMethod.GET)
+    public String paymentWx(final HttpServletRequest request, final HttpServletResponse response, final Model model,@RequestParam(value = "merchantId", required = true) long merchantId,@RequestParam(value = "name") String name) throws IOException {
+        model.addAttribute("mid", merchantId);
+        model.addAttribute("merchantName", name);
+        return "/payment-wx";
+    }
+
+    /**
+     * 扫固定码支付宝支付页面
+     * @param request
+     * @param response
+     * @param model
+     * @param merchantId
+     * @param name
+     * @return
+     * @throws IOException
+     */
+    @RequestMapping(value = "/paymentZfb", method = RequestMethod.GET)
+    public String paymentZfb(final HttpServletRequest request, final HttpServletResponse response, final Model model,@RequestParam(value = "merchantId", required = true) long merchantId,@RequestParam(value = "name") String name) throws IOException {
+        model.addAttribute("mid", merchantId);
+        model.addAttribute("merchantName", name);
+        return "/payment-zfb";
+    }
+}
