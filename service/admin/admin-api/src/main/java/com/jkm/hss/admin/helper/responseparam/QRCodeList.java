4ac21abacdacdd0fda1506271bbbc4f4ec570873
liucong <liucong@haodai.com>
2017-01-19 15:46:27 +0800

    account俩方法

--- service/admin/admin-api/src/main/java/com/jkm/hss/admin/helper/responseparam/QRCodeList.java
+++ service/admin/admin-api/src/main/java/com/jkm/hss/admin/helper/responseparam/QRCodeList.java
@@ -2,11 +2,13 @@ package com.jkm.hss.admin.helper.responseparam;
 
 import lombok.Data;
 
+import java.util.Date;
+
 /**
  * Created by Thinkpad on 2017/1/17.
  */
 @Data
 public class QRCodeList {
     private String code;
-    private String createtime;
+    private Date createtime;
 }
