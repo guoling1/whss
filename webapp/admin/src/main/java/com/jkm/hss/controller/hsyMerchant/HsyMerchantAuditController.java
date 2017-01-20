b22eadf16cdc0ab98f57de457b8eeb668eb47824
Administrator <706326182@qq.com>
2017-01-20 18:45:26 +0800

    hsy商户审核

--- webapp/admin/src/main/java/com/jkm/hss/controller/hsyMerchant/HsyMerchantAuditController.java
+++ webapp/admin/src/main/java/com/jkm/hss/controller/hsyMerchant/HsyMerchantAuditController.java
@@ -1,7 +1,49 @@
 package com.jkm.hss.controller.hsyMerchant;
 
+import com.jkm.base.common.entity.CommonResponse;
+import com.jkm.base.common.entity.PageModel;
+import com.jkm.hss.controller.BaseController;
+import com.jkm.hsy.user.constant.AppConstant;
+import com.jkm.hsy.user.entity.HsyMerchantAuditRequest;
+import com.jkm.hsy.user.entity.HsyMerchantAuditResponse;
+import com.jkm.hsy.user.service.HsyMerchantAuditService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
- * Created by Administrator on 2017/1/20.
+ * Created by zhangbin on 2017/1/20.
  */
-public class HsyMerchantAuditController {
+@Controller
+@RequestMapping(value = "/admin/hsyMerchantAudit")
+public class HsyMerchantAuditController extends BaseController {
+
+    @Autowired
+    private HsyMerchantAuditService hsyMerchantAuditService;
+
+    @ResponseBody
+    @RequestMapping(value = "/throughAudit",method = RequestMethod.POST)
+    public CommonResponse throughAudit(@RequestBody final HsyMerchantAuditRequest hsyMerchantAuditRequest){
+        final PageModel<HsyMerchantAuditResponse> pageModel = new PageModel<HsyMerchantAuditResponse>(hsyMerchantAuditRequest.getPageNo(), hsyMerchantAuditRequest.getPageSize());
+        hsyMerchantAuditRequest.setOffset(pageModel.getFirstIndex());
+        hsyMerchantAuditRequest.setStatus(AppConstant.SHOP_STATUS_NORMAL);
+        hsyMerchantAuditService.auditPass(hsyMerchantAuditRequest);
+        return CommonResponse.simpleResponse(CommonResponse.SUCCESS_CODE,"审核通过");
+
+    }
+
+
+    @ResponseBody
+    @RequestMapping(value = "/rejectToExamine",method = RequestMethod.POST)
+    public CommonResponse rejectToExamine(@RequestBody final HsyMerchantAuditRequest hsyMerchantAuditRequest){
+        final PageModel<HsyMerchantAuditResponse> pageModel = new PageModel<HsyMerchantAuditResponse>(hsyMerchantAuditRequest.getPageNo(), hsyMerchantAuditRequest.getPageSize());
+        hsyMerchantAuditRequest.setOffset(pageModel.getFirstIndex());
+        hsyMerchantAuditRequest.setStatus(AppConstant.SHOP_STATUS_REJECT);
+        hsyMerchantAuditService.auditPass(hsyMerchantAuditRequest);
+        return CommonResponse.simpleResponse(CommonResponse.SUCCESS_CODE,"审核未通过");
+
+    }
 }
