1e7f9ff6e55ff3a7cde5eb862b3ef184dbcd0afd
zhangyulong <yulong.zhang@jinkaimen.com>
2017-01-20 14:28:58 +0800

    hsy

--- webapp/admin/src/main/java/com/jkm/hss/controller/settle/SettleController.java
+++ webapp/admin/src/main/java/com/jkm/hss/controller/settle/SettleController.java
@@ -1,7 +1,10 @@
 package com.jkm.hss.controller.settle;
 
+import com.google.common.base.Function;
 import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
 import com.jkm.base.common.entity.CommonResponse;
+import com.jkm.base.common.entity.PageModel;
 import com.jkm.hss.account.entity.SettleAccountFlow;
 import com.jkm.hss.account.enums.EnumAccountFlowType;
 import com.jkm.hss.account.sevice.SettleAccountFlowService;
@@ -9,18 +12,26 @@ import com.jkm.hss.bill.service.OrderService;
 import com.jkm.hss.controller.BaseController;
 import com.jkm.hss.helper.request.BatchSettleRequest;
 import com.jkm.hss.helper.request.SettleRequest;
+import com.jkm.hss.helper.response.ListSettleAuditRecordResponse;
+import com.jkm.hss.product.enums.EnumProductType;
 import com.jkm.hss.settle.entity.AccountSettleAuditRecord;
+import com.jkm.hss.settle.enums.EnumAccountCheckStatus;
 import com.jkm.hss.settle.enums.EnumSettleOptionType;
+import com.jkm.hss.settle.enums.EnumSettleStatus;
+import com.jkm.hss.settle.helper.requestparam.ListSettleAuditRecordRequest;
 import com.jkm.hss.settle.service.AccountSettleAuditRecordService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 
@@ -29,7 +40,7 @@ import java.util.List;
  */
 @Slf4j
 @Controller
-@RequestMapping(value = "/settle")
+@RequestMapping(value = "/admin/settle")
 public class SettleController extends BaseController {
 
     @Autowired
@@ -154,5 +165,67 @@ public class SettleController extends BaseController {
             return CommonResponse.simpleResponse(-1, result.getRight());
         }
         return CommonResponse.simpleResponse(CommonResponse.SUCCESS_CODE, "success");
+    }
+
+    /**
+     * 结算审核记录list
+     *
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "list", method = RequestMethod.POST)
+    public CommonResponse listSettleAuditRecord(@RequestBody ListSettleAuditRecordRequest settleAuditRecordRequest) {
+        final PageModel<ListSettleAuditRecordResponse> result = new PageModel<>(settleAuditRecordRequest.getPageNo(), settleAuditRecordRequest.getPageSize());
+        if (StringUtils.isEmpty(settleAuditRecordRequest.getMerchantNo())) {
+            settleAuditRecordRequest.setMerchantNo(null);
+        }
+        if (StringUtils.isEmpty(settleAuditRecordRequest.getMerchantName())) {
+            settleAuditRecordRequest.setMerchantName(null);
+        }
+        if (StringUtils.isEmpty(settleAuditRecordRequest.getStartSettleDate()) || StringUtils.isEmpty(settleAuditRecordRequest.getEndSettleDate())) {
+            settleAuditRecordRequest.setStartSettleDate(null);
+            settleAuditRecordRequest.setEndSettleDate(null);
+        }
+        if (EnumAccountCheckStatus.DUE_ACCOUNT_CHECK.getId() != settleAuditRecordRequest.getCheckedStatus()
+                && EnumAccountCheckStatus.SIDE_EXCEPTION.getId() != settleAuditRecordRequest.getCheckedStatus()
+                && EnumAccountCheckStatus.SUCCESS_ACCOUNT_CHECK.getId() != settleAuditRecordRequest.getCheckedStatus()) {
+            settleAuditRecordRequest.setCheckedStatus(0);
+        }
+        if (EnumSettleStatus.DUE_SETTLE.getId() != settleAuditRecordRequest.getSettleStatus()
+                && EnumSettleStatus.SETTLE_PART.getId() != settleAuditRecordRequest.getSettleStatus()
+                && EnumSettleStatus.SETTLED_ALL.getId() != settleAuditRecordRequest.getSettleStatus()) {
+            settleAuditRecordRequest.setSettleStatus(0);
+        }
+        final PageModel<AccountSettleAuditRecord> page = this.accountSettleAuditRecordService.listByParam(settleAuditRecordRequest);
+        final List<AccountSettleAuditRecord> records = page.getRecords();
+        if (CollectionUtils.isEmpty(records)) {
+            result.setCount(0);
+            result.setRecords(Collections.<ListSettleAuditRecordResponse>emptyList());
+            return CommonResponse.objectResponse(CommonResponse.SUCCESS_CODE, "success", result);
+        }
+        final List<ListSettleAuditRecordResponse> responses = Lists.transform(records, new Function<AccountSettleAuditRecord, ListSettleAuditRecordResponse>() {
+            @Override
+            public ListSettleAuditRecordResponse apply(AccountSettleAuditRecord input) {
+                final ListSettleAuditRecordResponse response = new ListSettleAuditRecordResponse();
+                response.setId(input.getId());
+                response.setMerchantNo(input.getMerchantNo());
+                response.setMerchantName(input.getMerchantName());
+                response.setDealerNo(input.getDealerNo());
+                response.setDealerName(input.getDealerName());
+                response.setAppId(input.getAppId());
+                response.setAppName(StringUtils.isEmpty(input.getAppId()) ? "" : EnumProductType.of(input.getAppId()).getName());
+                response.setTradeDate(input.getTradeDate());
+                response.setTradeNumber(input.getTradeNumber());
+                response.setSettleAmount(input.getSettleAmount());
+                response.setCheckedStatus(input.getAccountCheckStatus());
+                response.setCheckedStatusValue(input.getAccountCheckStatus() > 0 ? EnumAccountCheckStatus.of(input.getAccountCheckStatus()).getValue() : "");
+                response.setSettleStatus(input.getSettleStatus());
+                response.setSettleStatusValue(input.getSettleStatus() > 0 ? EnumSettleStatus.of(input.getSettleStatus()).getValue() : "");
+                response.setSettleDate(input.getSettleDate());
+                return response;
+            }
+        });
+        result.setRecords(responses);
+        return CommonResponse.objectResponse(CommonResponse.SUCCESS_CODE, "success", result);
     }
 }
