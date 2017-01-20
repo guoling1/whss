d8f60766dc5011576cc5553e1552fd3762050cc5
Administrator <706326182@qq.com>
2017-01-20 18:38:40 +0800

    hsy商户审核

--- service/hsyuser/user-service/src/main/java/com/jkm/hsy/user/service/impl/HsyMerchantAuditServiceImpl.java
+++ service/hsyuser/user-service/src/main/java/com/jkm/hsy/user/service/impl/HsyMerchantAuditServiceImpl.java
@@ -1,7 +1,50 @@
 package com.jkm.hsy.user.service.impl;
 
+import com.jkm.hsy.user.dao.HsyMerchantAuditDao;
+import com.jkm.hsy.user.entity.HsyMerchantAuditRequest;
+import com.jkm.hsy.user.entity.HsyMerchantAuditResponse;
+import com.jkm.hsy.user.service.HsyMerchantAuditService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
- * Created by Administrator on 2017/1/20.
+ * Created by Administrator on 2017/1/19.
  */
-public class HsyMerchantAuditServiceImpl {
+@Service
+public class HsyMerchantAuditServiceImpl implements HsyMerchantAuditService {
+
+    @Autowired
+    private HsyMerchantAuditDao hsyMerchantAuditDao;
+
+    @Override
+    public List<HsyMerchantAuditResponse> getMerchant(HsyMerchantAuditRequest hsyMerchantAuditRequest) {
+
+        List<HsyMerchantAuditResponse> list = hsyMerchantAuditDao.getMerchant(hsyMerchantAuditRequest);
+        return list;
+    }
+
+    @Override
+    public HsyMerchantAuditResponse getDetails(Long id) {
+
+        HsyMerchantAuditResponse res = hsyMerchantAuditDao.getDetails(id);
+        return res;
+    }
+
+    @Override
+    public void auditPass(HsyMerchantAuditRequest hsyMerchantAuditRequest) {
+        hsyMerchantAuditDao.updateAuditPass(hsyMerchantAuditRequest);
+    }
+
+    @Override
+    public void auditNotPass(HsyMerchantAuditRequest hsyMerchantAuditRequest) {
+        hsyMerchantAuditDao.updateAuditNotPass(hsyMerchantAuditRequest);
+    }
+
+    @Override
+    public int getCount(HsyMerchantAuditRequest hsyMerchantAuditRequest) {
+        int count = hsyMerchantAuditDao.getCount(hsyMerchantAuditRequest);
+        return count;
+    }
 }
