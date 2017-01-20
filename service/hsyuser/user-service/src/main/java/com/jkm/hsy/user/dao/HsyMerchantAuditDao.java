80d87ef3d89c3e56f1e2f46ce66a39f5573d2c35
Administrator <706326182@qq.com>
2017-01-20 18:40:39 +0800

    hsy商户审核

--- service/hsyuser/user-service/src/main/java/com/jkm/hsy/user/dao/HsyMerchantAuditDao.java
+++ service/hsyuser/user-service/src/main/java/com/jkm/hsy/user/dao/HsyMerchantAuditDao.java
@@ -1,7 +1,44 @@
 package com.jkm.hsy.user.dao;
 
+import com.jkm.hsy.user.entity.HsyMerchantAuditRequest;
+import com.jkm.hsy.user.entity.HsyMerchantAuditResponse;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 /**
- * Created by Administrator on 2017/1/20.
+ * Created by Administrator on 2017/1/19.
  */
+@Repository
 public interface HsyMerchantAuditDao {
+
+    /**
+     * 查询商户列表
+     */
+    List<HsyMerchantAuditResponse> getMerchant(HsyMerchantAuditRequest hsyMerchantAuditRequest);
+
+    /**
+     * 查询商户详情
+     */
+    HsyMerchantAuditResponse getDetails(@Param("id") Long id);
+
+    /**
+     * 审核通过
+     */
+    void updateAuditPass(HsyMerchantAuditRequest hsyMerchantAuditRequest);
+
+    /**
+     * 审核不通过
+     */
+    void updateAuditNotPass(HsyMerchantAuditRequest hsyMerchantAuditRequest);
+
+    /**
+     * 查询总条数
+     * @param hsyMerchantAuditRequest
+     * @return
+     */
+    int getCount(HsyMerchantAuditRequest hsyMerchantAuditRequest);
+
+
 }
