153948988897a07d0c7bb31883794a0fc0d75960
zhangyulong <yulong.zhang@jinkaimen.com>
2017-01-20 14:27:05 +0800

    hsy

--- webapp/admin/src/main/java/com/jkm/hss/schedule/Task.java
+++ webapp/admin/src/main/java/com/jkm/hss/schedule/Task.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@Lazy(false)
 public class Task {
 
     @Autowired
@@ -25,7 +26,7 @@ public class Task {
      *
      * 周一至周五 2点0时0分
      */
-    @Scheduled(cron = "0 25 19 ? * MON-FRI")
+    @Scheduled(cron = "0 15 13 ? * MON-FRI")
     public void handleT1SettleTask() {
         log.info("结算审核定时任务--start");
         this.accountSettleAuditRecordService.handleT1SettleTask();
