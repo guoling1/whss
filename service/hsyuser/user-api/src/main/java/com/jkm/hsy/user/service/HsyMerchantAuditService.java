d3df10455a0b76983c86323a044f01655ab5e3f4
Administrator <706326182@qq.com>
2017-01-20 18:36:46 +0800

    hsy商户审核

--- service/hsyuser/user-api/src/main/java/com/jkm/hsy/user/service/HsyMerchantAuditService.java
+++ service/hsyuser/user-api/src/main/java/com/jkm/hsy/user/service/HsyMerchantAuditService.java
@@ -1,7 +1,41 @@
 package com.jkm.hsy.user.service;
 
+import com.jkm.hsy.user.entity.HsyMerchantAuditRequest;
+import com.jkm.hsy.user.entity.HsyMerchantAuditResponse;
+
+import java.util.List;
+
 /**
- * Created by Administrator on 2017/1/20.
+ * Created by Administrator on 2017/1/19.
  */
 public interface HsyMerchantAuditService {
+
+    /**
+     * 查询商户列表
+     */
+    List<HsyMerchantAuditResponse> getMerchant(HsyMerchantAuditRequest hsyMerchantAuditRequest);
+
+    /**
+     * 查询商户详情
+     */
+    HsyMerchantAuditResponse getDetails(Long id);
+
+    /**
+     * 审核通过
+     */
+    void auditPass(HsyMerchantAuditRequest hsyMerchantAuditRequest);
+
+    /**
+     * 审核不通过
+     */
+    void auditNotPass(HsyMerchantAuditRequest hsyMerchantAuditRequest);
+
+    /**
+     * 查询条总数
+     * @param hsyMerchantAuditRequest
+     * @return
+     */
+    int getCount(HsyMerchantAuditRequest hsyMerchantAuditRequest);
+
+
 }
