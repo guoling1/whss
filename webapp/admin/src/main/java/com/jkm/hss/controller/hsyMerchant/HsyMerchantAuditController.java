003aeda64cdaa4031521d1d4e075aff6cd6384ae
Administrator <706326182@qq.com>
2017-01-20 15:50:17 +0800

    hsy代理商审核

--- webapp/admin/src/main/java/com/jkm/hss/controller/hsyMerchant/HsyMerchantAuditController.java
+++ webapp/admin/src/main/java/com/jkm/hss/controller/hsyMerchant/HsyMerchantAuditController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by zhangbin on 2017/1/20.
@@ -22,6 +23,7 @@ public class HsyMerchantAuditController extends BaseController {
     @Autowired
     private HsyMerchantAuditService hsyMerchantAuditService;
 
+    @ResponseBody
     @RequestMapping(value = "/throughAudit",method = RequestMethod.POST)
     public CommonResponse throughAudit(@RequestBody final HsyMerchantAuditRequest hsyMerchantAuditRequest){
         final PageModel<HsyMerchantAuditResponse> pageModel = new PageModel<HsyMerchantAuditResponse>(hsyMerchantAuditRequest.getPageNo(), hsyMerchantAuditRequest.getPageSize());
