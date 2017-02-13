cd741dcadb8e0a5d8e80d20141698b76fcc75df4
xiang.yu <http://gitlab.jinkaimen.com>
2017-02-13 17:58:34 +0800

    hsy新增业务订单

---
+++ webapp/dealer_PC/src/main/java/com/jkm/hss/controller/MyProfitController/ProfitController.java
@@ -0,0 +1,15 @@
+package com.jkm.hss.controller.MyProfitController;
+
+import com.jkm.hss.controller.BaseController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by yuxiang on 2017-02-13.
+ */
+@RestController
+@RequestMapping(value = "/profit")
+public class ProfitController extends BaseController{
+
+
+}
