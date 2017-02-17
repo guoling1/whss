74d525e5dcc57365f813a15169c9a5e0a38263ca
xingliujie <java_xlj@163.com>
2017-02-17 15:24:06 +0800

    代理商

--- webapp/dealer_PC/src/main/java/com/jkm/hss/controller/DealerController/DealerController.java
+++ webapp/dealer_PC/src/main/java/com/jkm/hss/controller/DealerController/DealerController.java
@@ -118,9 +118,9 @@ public class DealerController extends BaseController {
             if(StringUtils.isBlank(secondLevelDealerAdd2Request.getEmail())) {
                 return CommonResponse.simpleResponse(-1, "联系邮箱不能为空");
             }
-            if(!ValidateUtils.isEmail(secondLevelDealerAdd2Request.getEmail())) {
-                return CommonResponse.simpleResponse(-1, "联系邮箱格式错误");
-            }
+//            if(!ValidateUtils.isEmail(secondLevelDealerAdd2Request.getEmail())) {
+//                return CommonResponse.simpleResponse(-1, "联系邮箱格式错误");
+//            }
             if(StringUtils.isBlank(secondLevelDealerAdd2Request.getBelongProvinceCode())) {
                 return CommonResponse.simpleResponse(-1, "所在省份编码不能为空");
             }
@@ -254,9 +254,9 @@ public class DealerController extends BaseController {
             if(StringUtils.isBlank(request.getEmail())) {
                 return CommonResponse.simpleResponse(-1, "联系邮箱不能为空");
             }
-            if(!ValidateUtils.isEmail(request.getEmail())) {
-                return CommonResponse.simpleResponse(-1, "联系邮箱格式错误");
-            }
+//            if(!ValidateUtils.isEmail(request.getEmail())) {
+//                return CommonResponse.simpleResponse(-1, "联系邮箱格式错误");
+//            }
 
             if(StringUtils.isBlank(request.getBelongProvinceCode())) {
                 return CommonResponse.simpleResponse(-1, "所在省份编码不能为空");
