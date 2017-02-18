37e29ab9d43798c85de37c845828b61123fc3745
于超 <chao.yu@jinkaimen.com>
2017-02-18 14:53:15 +0800

    去掉无用的链接

--- webapp/dealer_PC/src/main/webapp/dealer-pc/src/index.vue
+++ webapp/dealer_PC/src/main/webapp/dealer-pc/src/index.vue
@@ -4,12 +4,12 @@
     <header class="main-header">
 
       <!-- Logo -->
-      <a href="index2.html" class="logo">
+      <div class="logo">
         <!-- mini logo for sidebar mini 50x50 pixels -->
         <span class="logo-mini">钱</span>
         <!-- logo for regular state and mobile devices -->
         <span class="logo-lg">钱包++</span>
-      </a>
+      </div>
 
       <!-- Header Navbar: style can be found in header.less -->
       <nav class="navbar navbar-static-top">
