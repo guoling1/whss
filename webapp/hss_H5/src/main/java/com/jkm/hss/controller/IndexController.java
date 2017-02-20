204184cf4f1363ba1dfbea0d7df439461f5a0092
xingliujie <java_xlj@163.com>
2017-02-20 18:39:09 +0800

    支付成功前台通知

--- webapp/hss_H5/src/main/java/com/jkm/hss/controller/IndexController.java
+++ webapp/hss_H5/src/main/java/com/jkm/hss/controller/IndexController.java
@@ -6,6 +6,7 @@ import com.jkm.hss.merchant.helper.MerchantSupport;
 import com.jkm.hss.merchant.helper.WxConstants;
 import com.jkm.hss.merchant.service.MerchantInfoService;
 import com.jkm.hss.merchant.service.UserInfoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,10 +16,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 /**
  * Created by yuxiang on 2016/11/16
  */
+@Slf4j
 @Controller
 @RequestMapping(value = "")
 public class IndexController extends BaseController {
@@ -45,23 +49,35 @@ public class IndexController extends BaseController {
     @RequestMapping(value = "/reg", method = RequestMethod.GET)
     public String reg(final HttpServletRequest request, final HttpServletResponse response, final Model model,@RequestParam(value = "invite", required = true) String invite) {
         boolean isRedirect = false;
-        String ul = request.getRequestURI();
-        if(!super.isLogin(request)){
-            return "redirect:"+ WxConstants.WEIXIN_USERINFO+ul+ WxConstants.WEIXIN_USERINFO_REDIRECT;
-        }else {
+        if (!super.isLogin(request)) {
+            String requestUrl = "";
+            if (request.getQueryString() == null) {
+                requestUrl = "";
+            } else {
+                requestUrl = request.getQueryString();
+            }
+            log.info("跳转地址是{}", requestUrl);
+            try {
+                String encoderUrl = URLEncoder.encode(requestUrl, "UTF-8");
+                return "redirect:" + WxConstants.WEIXIN_DEALER_USERINFO + encoderUrl + WxConstants.WEIXIN_USERINFO_REDIRECT;
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            return null;
+        } else {
             String url = "";
             Optional<UserInfo> userInfoOptional = userInfoService.selectByOpenId(super.getOpenId(request));
             if (userInfoOptional.isPresent()) {
-                model.addAttribute("message","您已经注册过了\n" +
+                model.addAttribute("message", "您已经注册过了\n" +
                         "不能再被邀请注册");
                 url = "/message";
-            }else {
+            } else {
                 model.addAttribute("inviteCode", invite);
                 url = "/reg";
             }
-            if(isRedirect){
-                return "redirect:"+url;
-            }else{
+            if (isRedirect) {
+                return "redirect:" + url;
+            } else {
                 return url;
             }
         }
