488d5fa63c19abefd971bc282484444547c58381
guoling1 <646626114@qq.com>
2017-03-01 15:57:15 +0800

    新增通道

--- webapp/admin/src/main/webapp/hss-admin/src/components/Test.vue
+++ webapp/admin/src/main/webapp/hss-admin/src/components/Test.vue
@@ -1194,365 +1194,6 @@
   }
 </style>-->
 
-<!--新增通道-->
-<template lang="html">
-  <div id="passAdd">
-    <div style="margin: 15px 15px 150px;">
-      <div class="box tableTop">
-        <div class="box-header with-border" style="margin-bottom: 15px">
-          <h3 class="box-title" v-if="isShow">新增通道</h3>
-          <h3 class="box-title" v-if="!isShow">通道详情</h3>
-        </div>
-        <div class="">
-          <div class="table-responsive">
-            <el-row type="flex" class="row-bg" justify="center">
-              <el-col :span="4">
-                <div class="alignRight">通道名称:</div>
-              </el-col>
-              <el-col :span="6">
-                <div class="grid-content bg-purple-light">
-                  <el-input size="small" v-model="name" placeholder="请输入内容"></el-input>
-                </div>
-              </el-col>
-              <el-col :span="8">
-                <div class="grid-content bg-purple-light right">例如：华有支付宝</div>
-              </el-col>
-            </el-row>
-            <el-row type="flex" class="row-bg" justify="center">
-              <el-col :span="4">
-                <div class="alignRight">通道编码:</div>
-              </el-col>
-              <el-col :span="6">
-                <div class="grid-content bg-purple-light">
-                  <el-input size="small" v-model="query.channelCode" placeholder="请输入内容"></el-input>
-                </div>
-              </el-col>
-              <el-col :span="8">
-                <div class="grid-content bg-purple-light right">例如：SM_Alipay</div>
-              </el-col>
-            </el-row>
-            <el-row type="flex" class="row-bg" justify="center">
-              <el-col :span="4">
-                <div class="alignRight">支付方式:</div>
-              </el-col>
-              <el-col :span="6">
-                <div class="grid-content bg-purple-light">
-                  <el-checkbox-group v-model="payWay">
-                    <el-checkbox label="微信公众号" v-if="nameType=='wx'||nameType==''"></el-checkbox>
-                    <el-checkbox label="微信公众号" v-if="nameType=='zfb'" disabled></el-checkbox>
-                    <el-checkbox label="微信扫码" v-if="nameType=='wx'||nameType==''"></el-checkbox>
-                    <el-checkbox label="微信扫码" v-if="nameType=='zfb'" disabled></el-checkbox>
-                    <el-checkbox label="支付宝公众号" v-if="nameType=='zfb'||nameType==''"></el-checkbox>
-                    <el-checkbox label="支付宝公众号" v-if="nameType=='wx'" disabled></el-checkbox>
-                    <el-checkbox label="支付宝扫码" v-if="nameType=='zfb'||nameType==''"></el-checkbox>
-                    <el-checkbox label="支付宝扫码" v-if="nameType=='wx'" disabled></el-checkbox>
-                  </el-checkbox-group>
-                </div>
-              </el-col>
-              <el-col :span="8"></el-col>
-            </el-row>
-            <el-row type="flex" class="row-bg" justify="center">
-              <el-col :span="4">
-                <div class="alignRight">收单机构:</div>
-              </el-col>
-              <el-col :span="6">
-                <div class="grid-content bg-purple-light">
-                  <el-select style="width: 100%" v-model="query.thirdCompany" clearable placeholder="请选择" size="small">
-                    <el-option label="微信" value="微信">微信</el-option>
-                    <el-option label="支付宝" value="支付宝">支付宝</el-option>
-                    <el-option label="京东钱包" value="京东钱包">京东钱包</el-option>
-                    <el-option label="百度钱包" value="百度钱包">百度钱包</el-option>
-                  </el-select>
-                </div>
-              </el-col>
-              <el-col :span="8">
-                <div class="grid-content bg-purple-light right">例如：支付宝、微信、京东钱包、百度钱包</div>
-              </el-col>
-            </el-row>
-            <el-row type="flex" class="row-bg" justify="center">
-              <el-col :span="4">
-                <div class="alignRight">渠道来源:</div>
-              </el-col>
-              <el-col :span="6">
-                <div class="grid-content bg-purple-light">
-                  <el-input size="small" v-model="query.channelSource" placeholder="数字或字母的组合，4-20位"></el-input>
-                </div>
-              </el-col>
-              <el-col :span="8">
-                <div class="grid-content bg-purple-light right">例如：华有，显示给用户</div>
-              </el-col>
-            </el-row>
-            <el-row type="flex" class="row-bg" justify="center">
-              <el-col :span="4">
-                <div class="alignRight">支付费率:</div>
-              </el-col>
-              <el-col :span="6">
-                <div class="grid-content bg-purple-light" style="position: relative">
-                  <el-input size="small" v-model="query.basicTradeRate" placeholder="请输入内容"></el-input>
-                  <b>%</b>
-                </div>
-              </el-col>
-              <el-col :span="8">
-                <div class="grid-content bg-purple-light right">例如：0.3%</div>
-              </el-col>
-            </el-row>
-            <el-row type="flex" class="row-bg" justify="center">
-              <el-col :span="4">
-                <div class="alignRight">打款费用:</div>
-              </el-col>
-              <el-col :span="6">
-                <div class="grid-content bg-purple-light" style="position: relative">
-                  <el-input size="small" v-model="query.basicWithdrawFee" placeholder="请输入内容"></el-input>
-                  <b>元/笔</b>
-                </div>
-              </el-col>
-              <el-col :span="8">
-                <div class="grid-content bg-purple-light right">例如：0.5元/笔</div>
-              </el-col>
-            </el-row>
-            <el-row type="flex" class="row-bg" justify="center">
-              <el-col :span="4">
-                <div class="alignRight">结算时间:</div>
-              </el-col>
-              <el-col :span="6">
-                <div class="grid-content bg-purple-light">
-                  <el-select style="width: 100%" v-model="query.basicBalanceType" clearable placeholder="请选择" size="small">
-                    <el-option label="D0" value="D0">D0</el-option>
-                    <el-option label="D1" value="D1">D1</el-option>
-                    <el-option label="T0" value="T0">T0</el-option>
-                    <el-option label="T1" value="T1">T1</el-option>
-                  </el-select>
-                </div>
-              </el-col>
-              <el-col :span="8">
-                <div class="grid-content bg-purple-light" style="margin: 0 15px;">
-                </div>
-              </el-col>
-            </el-row>
-            <el-row type="flex" class="row-bg" justify="center">
-              <el-col :span="4">
-                <div class="alignRight">结算方式:</div>
-              </el-col>
-              <el-col :span="6">
-                <div class="grid-content bg-purple-light">
-                  <el-select style="width: 100%" v-model="query.basicSettleType" clearable placeholder="请选择" size="small">
-                    <el-option label="通道自动结算" value="AUTO">通道自动结算</el-option>
-                    <el-option label="自主打款结算" value="SELF">自主打款结算</el-option>
-                  </el-select>
-                </div>
-              </el-col>
-              <el-col :span="8">
-                <div class="grid-content bg-purple-light" style="margin: 0 15px;">
-                </div>
-              </el-col>
-            </el-row>
-            <el-row type="flex" class="row-bg" justify="center">
-              <el-col :span="4">
-                <div class="alignRight">预估额度:</div>
-              </el-col>
-              <el-col :span="6">
-                <div class="grid-content bg-purple-light" style="position: relative">
-                  <el-input size="small" v-model="query.limitAmount" placeholder="请输入内容"></el-input>
-                  <b>元/笔</b>
-                </div>
-              </el-col>
-              <el-col :span="8">
-                <div class="grid-content bg-purple-light"></div>
-              </el-col>
-            </el-row>
-            <el-row type="flex" class="row-bg" justify="center">
-              <el-col :span="4">
-                <div class="alignRight">一户一报:</div>
-              </el-col>
-              <el-col :span="6">
-                <div class="grid-content bg-purple-light">
-                  <el-radio class="radio" v-model="query.isNeed" label="1">支持</el-radio>
-                  <el-radio class="radio" v-model="query.isNeed" label="2">不支持</el-radio>
-                </div>
-              </el-col>
-              <el-col :span="8">
-                <div class="grid-content bg-purple-light"></div>
-              </el-col>
-            </el-row>
-            <el-row type="flex" class="row-bg" justify="center">
-              <el-col :span="4">
-                <div class="alignRight">备注信息:</div>
-              </el-col>
-              <el-col :span="6">
-                <div class="grid-content bg-purple-light">
-                  <el-input type="textarea" :autosize="{ minRows: 2, maxRows: 4}" size="small" v-model="query.remarks" placeholder="请输入内容"></el-input>
-                </div>
-              </el-col>
-              <el-col :span="8">
-                <div class="grid-content bg-purple-light"></div>
-              </el-col>
-            </el-row>
-          </div>
-        </div>
-        <el-row type="flex" class="row-bg" justify="center">
-          <el-col :span="4">
-            <div class="alignRight"></div>
-          </el-col>
-          <el-col :span="6">
-            <div class="grid-content bg-purple-light" style="width: 100%">
-              <div class="btn btn-primary" @click="goBack" style="width: 45%;margin: 20px 0 100px;">
-                返回
-              </div>
-              <div class="btn btn-primary" @click="create" v-if="isShow" style="width: 45%;float: right;margin: 20px 0 100px;">
-                创建代理商
-              </div>
-              <div class="btn btn-primary" @click="change()" v-if="!isShow" style="width: 45%;float: right;margin: 20px 0 100px;">
-                修改
-              </div>
-            </div>
-          </el-col>
-          <el-col :span="8">
-            <div class="grid-content bg-purple-light"></div>
-          </el-col>
-        </el-row>
-      </div>
-    </div>
-  </div>
-</template>
-
-<script lang="babel">
-  export default {
-    name: 'passAdd',
-    data () {
-      return {
-        query: {
-          channelName: '',
-          channelCode: '',
-          supportWay:'',
-          thirdCompany:'',
-          channelSource:'',
-          basicTradeRate:'',
-          basicWithdrawFee:'',
-          basicBalanceType: '',
-          basicSettleType: '',
-          limitAmount: '',
-          isNeed: '',
-          remarks: ''
-        },
-        id: 0,
-        isShow: true,
-        name:'',
-        payWay:[],
-        nameType:''
-      }
-    },
-    created: function () {
-      //若为查看详情
-      if (this.$route.query.id != undefined) {
-        this.$data.isShow = false;
-        this.$http.get('/admin/dealer/findBydealerId/' + this.$route.query.id)
-          .then(function (res) {
-            this.$data.query = res.data;
-            this.$data.province = res.data.belongProvinceName;
-            this.$data.city = res.data.belongCityName;
-          })
-      }
-    },
-    methods: {
-      //创建一级代理
-      create: function () {
-        this.query.channelName = this.name;
-        if(this.payWay.length == 2){
-          this.query.supportWay = 3;
-        }else if(this.payWay.length == 1){
-          if(/公众号/.test(this.payWay[0])){
-            this.query.supportWay = 2;
-          }else if(/扫码/.test(this.payWay[0])){
-            this.query.supportWay = 1;
-          }
-        }
-        this.$http.post('/admin/paymentChannel/add', this.$data.query)
-          .then(function (res) {
-            this.$message({
-              showClose: true,
-              message: '创建成功',
-              type: 'success'
-            });
-            this.$router.push('/admin/record/passList')
-          }, function (err) {
-            this.$message({
-              showClose: true,
-              message: err.statusMessage,
-              type: 'error'
-            });
-          })
-      },
-      goBack: function () {
-        this.$router.push('/admin/record/passList')
-      },
-      //修改
-      change: function () {
-        this.$data.query.dealerId = this.$data.query.id;
-        this.$http.post('/admin/user/updateDealer2', this.$data.query)
-          .then(function (res) {
-            this.$message({
-              showClose: true,
-              message: '修改成功',
-              type: 'success'
-            });
-            if(this.$route.query.level==2){
-              this.$router.push('/admin/record/agentListSec')
-            }else {
-              this.$router.push('/admin/record/agentListFir')
-            }
-          }, function (err) {
-            this.$message({
-              showClose: true,
-              message: err.statusMessage,
-              type: 'error'
-            });
-          })
-      }
-    },
-    watch:{
-      name: function (val) {
-        if(/微信/.test(val)){
-          this.payWay = []
-          this.nameType = 'wx'
-        }else if(/支付宝/.test(val)){
-          this.payWay = []
-          this.nameType = 'zfb'
-        }else {
-          this.payWay = []
-          this.nameType = ''
-        }
-      }
-    }
-  }
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="less">
-  .alignRight {
-    margin-right: 15px;
-    text-align: right;
-    height: 30px;
-    line-height: 30px;
-    font-weight: bold;
-    margin-bottom: 10px;
-  }
-
-  .right{
-    height: 30px;
-    line-height: 30px;
-    margin-left: 15px;
-    color: #999999;
-  }
-  b{
-    height: 30px;
-    line-height: 30px;
-    margin-right: 15px;
-    position: absolute;
-    top:0;
-    right: 0;
-  }
-</style>
-
 <!--通道列表-->
 <!--<template>
   <div id="passList">
@@ -1771,7 +1412,7 @@
 </style>-->
 
 <!--新增产品-->
-<!--<template lang="html">
+<template lang="html">
   <div id="productAdd">
     <div style="margin: 15px 15px 150px;">
       <div class="box tableTop">
@@ -1822,7 +1463,7 @@
                     <el-table-column property="name" label="姓名" width="200"></el-table-column>
                     <el-table-column property="address" label="地址"></el-table-column>
                   </el-table>
-                  <span class="btn btn-primary" style="margin: 15px 0" @click="dialogTableVisible = true" >添加通道</span>
+                  <span class="btn btn-primary" style="margin: 15px 0" @click="passSelect">添加通道</span>
                 </div>
               </el-col>
               <el-col :span="2"></el-col>
@@ -1830,13 +1471,8 @@
 
             <el-dialog title="选择通道" v-model="dialogTableVisible">
               <el-table :data="gridData" @selection-change="handleSelectionChange" border>
-                <el-table-column
-                  type="selection"
-                  width="55">
-                </el-table-column>
-                <el-table-column property="date" label="日期" width="150"></el-table-column>
-                <el-table-column property="name" label="姓名" width="200"></el-table-column>
-                <el-table-column property="address" label="地址"></el-table-column>
+                <el-table-column type="selection" width="55"></el-table-column>
+                <el-table-column property="address" label="通道名称"></el-table-column>
               </el-table>
               <div slot="footer" class="dialog-footer">
                 <el-button @click="dialogTableVisible = false">取 消</el-button>
@@ -1935,7 +1571,8 @@
     data () {
       return {
         tableHas:false,
-        dialogTableVisible: false,
+        dialogTableVisible: false, // 选择通道层
+        gridData: [], //选择通道时的列表
         multipleSelection:'',
         password:'',
         query: {
@@ -1978,6 +1615,21 @@
       }
     },
     methods: {
+      // 添加通道
+      passSelect: function () {
+        this.$http.get('/admin/product/listChannel')
+          .then(res => {
+            this.gridData = res.data;
+            this.dialogTableVisible = true;
+          })
+          .catch(err =>{
+            this.$message({
+              showClose: true,
+              message: err.statusMessage,
+              type: 'error'
+            })
+          })
+      },
       handleSelectionChange(val) {
         this.multipleSelection = val;
         console.log(this.multipleSelection)
@@ -2054,7 +1706,7 @@
   }
 </script>
 
-&lt;!&ndash; Add "scoped" attribute to limit CSS to this component only &ndash;&gt;
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
   .alignRight {
     margin-right: 15px;
@@ -2079,7 +1731,7 @@
     top:0;
     right: 0;
   }
-</style>-->
+</style>
 
 <!--产品列表-->
 <!--<template>
