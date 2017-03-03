cbad6dcf7c763a18859a50d5940b0307ba16911d
Administrator <706326182@qq.com>
2017-03-03 13:26:44 +0800

    发布公告

---
+++ service/merchant/merchant-api/src/main/java/com/jkm/hss/merchant/entity/Notice.java
@@ -0,0 +1,46 @@
+package com.jkm.hss.merchant.entity;
+
+import com.jkm.base.common.entity.BaseEntity;
+import lombok.Data;
+
+/**
+ * Created by zhangbin on 2017/3/2.
+ */
+@Data
+public class Notice extends BaseEntity {
+
+    /**
+     * 发布人
+     */
+    private String publisher;
+
+    /**
+     * 产品id
+     */
+    private int productId;
+
+    /**
+     * 产品名称
+     */
+    private String productName;
+
+    /**
+     * 产品类型
+     */
+    private String productType;
+
+    /**
+     * 发布标题
+     */
+    private String title;
+
+    /**
+     * 正文
+     */
+    private String text;
+
+    /**
+     * 过期时间
+     */
+    private Data expirationTime;
+}
---
+++ service/merchant/merchant-api/src/main/java/com/jkm/hss/merchant/entity/NoticeResponse.java
@@ -0,0 +1,77 @@
+package com.jkm.hss.merchant.entity;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * Created by zhangbin on 2017/3/2.
+ */
+@Data
+public class NoticeResponse {
+    /**
+     * 公告id
+     */
+    private int id;
+
+    /**
+     * 发布时间
+     */
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    private Date updateTime;
+
+    /**
+     * 状态
+     * tinyint
+     */
+    private int status;
+
+    /**
+     *  发布状态
+     */
+    private String pushStatus;
+
+    /**
+     * 发布人
+     */
+    private String publisher;
+
+    /**
+     * 产品id
+     */
+    private int productId;
+
+    /**
+     * 产品名称
+     */
+    private String productName;
+
+    /**
+     * 产品类型
+     */
+    private String productType;
+
+    /**
+     * 发布标题
+     */
+    private String title;
+
+    /**
+     * 正文
+     */
+    private String text;
+
+    /**
+     * 过期时间
+     */
+    private Date expirationTime;
+
+    /**
+     * 发布时间
+     */
+    private String dates;
+}
