566564ea4913cbe6ba38336cc6a92b03e6a56f3d
xingliujie <java_xlj@163.com>
2017-03-03 17:16:24 +0800

    代理商修改

---
+++ webapp/dealer_PC/src/main/java/com/jkm/hss/controller/DealerController/MerchantController.java
@@ -0,0 +1,29 @@
+package com.jkm.hss.controller.DealerController;
+
+import com.jkm.base.common.entity.CommonResponse;
+import com.jkm.hss.controller.BaseController;
+import com.jkm.hss.helper.request.MerchantListRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by xingliujie on 2017/3/3.
+ */
+@Slf4j
+@RestController
+@RequestMapping(value = "/daili/merchant")
+public class MerchantController extends BaseController {
+    /**
+     * 商户列表
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "/merchantList", method = RequestMethod.POST)
+    public CommonResponse merchantList(final HttpServletResponse response,@RequestBody final MerchantListRequest merchantListRequest) {
+
+        long dealerId = super.getDealerId();
+        return CommonResponse.objectResponse(CommonResponse.SUCCESS_CODE, "查询成功", "");
+    }
+}
