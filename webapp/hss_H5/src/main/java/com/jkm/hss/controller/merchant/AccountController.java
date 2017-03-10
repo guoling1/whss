1bdcb0ad0477cdcb09f4fd15e15a824081beace1
xiang.yu <http://gitlab.jinkaimen.com>
2017-03-10 15:23:59 +0800

    dealer pc bug

--- webapp/hss_H5/src/main/java/com/jkm/hss/controller/merchant/AccountController.java
+++ webapp/hss_H5/src/main/java/com/jkm/hss/controller/merchant/AccountController.java
@@ -78,7 +78,7 @@ public class AccountController extends BaseController{
      * 跳到提现页面
      * @return
      */
-    @RequestMapping(value = "/toWithdraw", method = RequestMethod.POST)
+    @RequestMapping(value = "/toWithdraw", method = RequestMethod.GET)
     public String toWithdrawJsp(HttpServletRequest request){
 
         return "/withdraw";
@@ -88,7 +88,7 @@ public class AccountController extends BaseController{
      * 跳到余额页面
      * @return
      */
-    @RequestMapping(value = "/toHssAccount", method = RequestMethod.POST)
+    @RequestMapping(value = "/toHssAccount", method = RequestMethod.GET)
     public String toHssAccount(HttpServletRequest request){
 
         return "/hssAccount";
@@ -98,7 +98,7 @@ public class AccountController extends BaseController{
      * 跳到余额流水详情
      * @return
      */
-    @RequestMapping(value = "/toHssAccountFlow", method = RequestMethod.POST)
+    @RequestMapping(value = "/toHssAccountFlow", method = RequestMethod.GET)
     public String toHssAccountFlow(HttpServletRequest request){
 
         return "/hssAccountFlow";
@@ -108,7 +108,7 @@ public class AccountController extends BaseController{
      * 跳到提现页面
      * @return
      */
-    @RequestMapping(value = "/toHssWithdrawSuccess", method = RequestMethod.POST)
+    @RequestMapping(value = "/toHssWithdrawSuccess", method = RequestMethod.GET)
     public String toHssWithdrawSuccess(HttpServletRequest request){
 
         return "/hssWithdrawSuccess";
