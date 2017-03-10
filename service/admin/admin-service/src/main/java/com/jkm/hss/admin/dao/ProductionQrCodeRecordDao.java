f4ab175586d333c36766b8d38742d037c994be95
xingliujie <java_xlj@163.com>
2017-03-10 19:48:32 +0800

    修改卡宾查询

--- service/admin/admin-service/src/main/java/com/jkm/hss/admin/dao/ProductionQrCodeRecordDao.java
+++ service/admin/admin-service/src/main/java/com/jkm/hss/admin/dao/ProductionQrCodeRecordDao.java
@@ -1,7 +1,11 @@
 package com.jkm.hss.admin.dao;
 
 import com.jkm.hss.admin.entity.ProductionQrCodeRecord;
+import com.jkm.hss.admin.helper.requestparam.ProductionRequest;
+import com.jkm.hss.admin.helper.responseparam.ProductionListResponse;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by xingliujie on 2017/2/20.
@@ -14,4 +18,18 @@ public interface ProductionQrCodeRecordDao {
      * @param productionQrCodeRecord
      */
     void insert(ProductionQrCodeRecord productionQrCodeRecord);
+
+    /**
+     * 按条件查询列表
+     *
+     * @param productionRequest
+     */
+    List<ProductionListResponse> selectList(ProductionRequest productionRequest);
+
+    /**
+     * 按条件查询列表个数
+     *
+     * @param productionRequest
+     */
+    Long selectCount(ProductionRequest productionRequest);
 }
