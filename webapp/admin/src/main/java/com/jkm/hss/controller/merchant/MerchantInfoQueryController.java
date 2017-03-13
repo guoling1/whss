ca20ac4634c37b6d12862116268d8c6af0233845
Administrator <706326182@qq.com>
2017-03-13 17:00:51 +0800

    商户详情

--- webapp/admin/src/main/java/com/jkm/hss/controller/merchant/MerchantInfoQueryController.java
+++ webapp/admin/src/main/java/com/jkm/hss/controller/merchant/MerchantInfoQueryController.java
@@ -44,48 +44,6 @@ public class MerchantInfoQueryController extends BaseController {
     @Autowired
     private OSSClient ossClient;
 
-//    @ResponseBody
-//    @RequestMapping(value = "/getAll",method = RequestMethod.POST)
-//    public CommonResponse<BaseEntity> getAll(@RequestBody MerchantInfoRequest req) throws ParseException {
-//        final PageModel<MerchantInfoResponse> pageModel = new PageModel<MerchantInfoResponse>(req.getPageNo(), req.getPageSize());
-//        req.setOffset(pageModel.getFirstIndex());
-//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-//        if(req.getEndTime()!=null&&!"".equals(req.getEndTime())){
-//            Date dt = sdf.parse(req.getEndTime());
-//            Calendar rightNow = Calendar.getInstance();
-//            rightNow.setTime(dt);
-//            rightNow.add(Calendar.DATE, 1);
-//            req.setEndTime(sdf.format(rightNow.getTime()));
-//        }
-//        if(req.getEndTime1()!=null&&!"".equals(req.getEndTime1())){
-//            Date dt = sdf.parse(req.getEndTime1());
-//            Calendar rightNow = Calendar.getInstance();
-//            rightNow.setTime(dt);
-//            rightNow.add(Calendar.DATE, 1);
-//            req.setEndTime1(sdf.format(rightNow.getTime()));
-//        }
-//        if(req.getEndTime2()!=null&&!"".equals(req.getEndTime2())){
-//            Date dt = sdf.parse(req.getEndTime2());
-//            Calendar rightNow = Calendar.getInstance();
-//            rightNow.setTime(dt);
-//            rightNow.add(Calendar.DATE, 1);
-//            req.setEndTime2(sdf.format(rightNow.getTime()));
-//        }
-//        long count = this.merchantInfoQueryService.getCount(req);
-//        List<MerchantInfoResponse> list = this.merchantInfoQueryService.getAll(req);
-//        if (list == null){
-//            return CommonResponse.simpleResponse(-1,"未查到相关数据");
-//        }else {
-//            for (int i=0;i<list.size();i++){
-//                list.get(i).setMobile(MerchantSupport.decryptMobile(list.get(i).getMobile()));
-//            }
-//        }
-//
-//        pageModel.setCount(count);
-//        pageModel.setRecords(list);
-//        return CommonResponse.objectResponse(1, "success", pageModel);
-//    }
-
     @ResponseBody
     @RequestMapping(value = "/getAll",method = RequestMethod.POST)
     public CommonResponse<BaseEntity> getAll(@RequestBody MerchantInfoRequest req) throws ParseException {
