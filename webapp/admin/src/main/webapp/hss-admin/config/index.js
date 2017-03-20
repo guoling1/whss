b9a84445986f4fd91219829539092b8db787452e
guoling1 <646626114@qq.com>
2017-03-20 15:47:30 +0800

    商户时间修改

--- webapp/admin/src/main/webapp/hss-admin/config/index.js
+++ webapp/admin/src/main/webapp/hss-admin/config/index.js
@@ -6,7 +6,7 @@ module.exports = {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../../WEB-INF/index.html'),
     assetsRoot: path.resolve(__dirname, '../../'),
-    assetsSubDirectory: 'static1.0.15',
+    assetsSubDirectory: 'static1.0.18',
     assetsPublicPath: 'http://img.jinkaimen.cn/admin/hss/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
