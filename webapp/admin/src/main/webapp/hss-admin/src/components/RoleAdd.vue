6f6a1390dc43e3c02905622bceb52551646ed2ef
guoling1 <646626114@qq.com>
2017-03-23 15:13:38 +0800

    菜单调整+分润修改

--- webapp/admin/src/main/webapp/hss-admin/src/components/RoleAdd.vue
+++ webapp/admin/src/main/webapp/hss-admin/src/components/RoleAdd.vue
@@ -6,14 +6,13 @@
           <h3 class="box-title">角色管理</h3>
         </div>
         <div class="box-body">
-          <!--筛选-->
           <ul>
             <li class="same">
-              <label>角色名称:</label>
+              <label class="title">角色名称:</label>
               <el-input style="width: 220px" placeholder="请输入内容" size="small"></el-input>
             </li>
             <li class="same">
-              <label>角色名称:</label>
+              <label class="title">权限:</label>
               <table style="width:90%;display: inline-table;vertical-align: top" border="0" cellspacing="0" cellpadding="0" class="table table-bordered">
                 <thead>
                 <tr>
@@ -29,7 +28,7 @@
                     <table style="width: 100%;height: 100%" border="0" cellspacing="0" cellpadding="0" class="table table-bordered">
                       <tbody>
                       <tr v-for="itemSon in item.son">
-                        <td>{{itemSon.name}}</td>
+                        <td><el-checkbox style="margin: 0 5px" v-model="itemSon.isTrue"></el-checkbox>{{itemSon.name}}</td>
                       </tr>
                       </tbody>
                     </table>
@@ -39,17 +38,20 @@
                       <tbody>
                       <tr width="100%" v-for="itemSon in item.son">
                         <td>
-                          <span v-for="itemChild in itemSon.children">{{itemChild.name}}</span>
+                          <span v-for="itemChild in itemSon.children" style="margin-right: 10px"><el-checkbox style="margin: 0 5px" v-model="itemChild.isTrue"></el-checkbox>{{itemChild.name}}</span>
                         </td>
                       </tr>
                       </tbody>
-
                     </table>
                   </td>
                 </tr>
                 </tbody>
 
               </table>
+            </li>
+            <li class="same">
+              <label class="title"></label>
+              <el-button type="primary" @click="submit">确 定</el-button>
             </li>
           </ul>
         </div>
@@ -64,7 +66,6 @@
       return {
         tableData: [
           {
-
             name: '员工权限管理',
             isTrue: true,
             son: [{
@@ -81,36 +82,35 @@
               name: '新增员工',
               isTrue: false,
               children: [{
-                name: '禁用',
+                name: '禁用1',
                 isTrue: false,
               }, {
-                name: '编辑',
+                name: '编辑1',
                 isTrue: false,
               }]
             }]
-
           },
           {
             name: '交易查询',
             isTrue: false,
             son: [{
-              name: '员工管理',
+              name: '员工管理1',
               isTrue: false,
               children: [{
-                name: '禁用',
+                name: '禁用2',
                 isTrue: false,
               }, {
-                name: '编辑',
+                name: '编辑2',
                 isTrue: false,
               }]
             }, {
               name: '新增员工',
               isTrue: false,
               children: [{
-                name: '禁用',
+                name: '禁用3',
                 isTrue: false,
               }, {
-                name: '编辑',
+                name: '编辑3',
                 isTrue: false,
               }]
             }]
@@ -121,17 +121,26 @@
     created: function () {
 
     },
-    methods: {},
+    methods: {
+      submit:function () {
+        console.log(this.tableData)
+      }
+    },
     watch: {},
   }
 </script>
-<style scoped lang="less">
+<style scoped lang="less" rel="stylesheet/less">
   ul{
     padding: 0;
   }
   .same{
     list-style: none;
     margin: 0 15px 15px 0;
+    .title{
+      width: 80px;
+      margin-right: 15px;
+      text-align: right;
+    }
   }
   .btn{
     font-size: 12px;
