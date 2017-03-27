6897d02798e8140ee3809a33c4604507a340fe44
guoling1 <646626114@qq.com>
2017-03-27 19:30:00 +0800

    上线地址修改

--- webapp/admin/src/main/webapp/hss-admin/src/components/newVersion/NewWithdrawalQuery.vue
+++ webapp/admin/src/main/webapp/hss-admin/src/components/newVersion/NewWithdrawalQuery.vue
@@ -158,15 +158,15 @@
         pageTotal: 0,
         addTotal: 0,
         //正式
-        /*queryUrl:'http://pay.qianbaojiajia.com/order/withdraw/listOrder',
+        queryUrl:'http://pay.qianbaojiajia.com/order/withdraw/listOrder',
          excelUrl:'http://pay.qianbaojiajia.com/order/withdraw/exportExcel',
          syncUrl:'http://pay.qianbaojiajia.com/order/syncWithdrawOrder',
-         addUrl:'http://pay.qianbaojiajia.com/order/withdraw/countAmount',*/
+         addUrl:'http://pay.qianbaojiajia.com/order/withdraw/countAmount',
         //测试
-        queryUrl:'http://192.168.1.20:8076/order/withdraw/listOrder',
+        /*queryUrl:'http://192.168.1.20:8076/order/withdraw/listOrder',
         excelUrl:'http://192.168.1.20:8076/order/withdraw/exportExcel',
         syncUrl:'http://192.168.1.20:8076/order/syncWithdrawOrder',
-        addUrl:'http://192.168.1.25:8240/order/withdraw/countAmount',
+        addUrl:'http://192.168.1.25:8240/order/withdraw/countAmount',*/
       }
     },
     created: function () {
