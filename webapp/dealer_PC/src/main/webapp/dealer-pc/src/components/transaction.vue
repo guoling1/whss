c667febecd30829f14fdd4e5df974112774f17f8
guoling1 <646626114@qq.com>
2017-03-27 19:23:42 +0800

    交易接口修改

--- webapp/dealer_PC/src/main/webapp/dealer-pc/src/components/transaction.vue
+++ webapp/dealer_PC/src/main/webapp/dealer-pc/src/components/transaction.vue
@@ -79,7 +79,7 @@
               </div>
             </div>
             <div class="box-body">
-              <el-table v-loading.body="tableLoading" height="583" style="font-size: 12px;margin:15px 0" :data="records" border :row-style="tableFoot">
+              <el-table v-loading.body="tableLoading" height="583" style="font-size: 12px;margin:15px 0" :data="records" border>
                 <el-table-column type="index" width="62" label="序号" fixed="left"></el-table-column>
                 <el-table-column prop="appId" label="业务方" min-width="85"></el-table-column>
                 <el-table-column label="业务订单号" min-width="112">
