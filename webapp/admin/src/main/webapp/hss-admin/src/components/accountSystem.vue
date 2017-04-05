f08afd7ffe37797722400f915ca24d1198a24a69
guoling1 <646626114@qq.com>
2017-04-05 18:43:36 +0800

    对账传文件bug

--- webapp/admin/src/main/webapp/hss-admin/src/components/accountSystem.vue
+++ webapp/admin/src/main/webapp/hss-admin/src/components/accountSystem.vue
@@ -56,9 +56,9 @@
             </el-table-column>
             <el-table-column label="操作">
               <template scope="scope">
-                  <el-button @click="handle" type="text" size="small" v-if="scope.row.batchNO==undefined">上传文件</el-button>
+                  <el-button @click="handle(scope.row.id)" type="text" size="small" v-if="scope.row.batchNO==undefined">上传文件</el-button>
                   <el-button @click="load(scope.row.batchNO)" type="text" size="small" v-if="scope.row.batchNO!=undefined">下载文件</el-button>
-                  <el-button @click="handle" type="text" size="small" v-if="scope.row.batchNO!=undefined">重新上传</el-button>
+                  <el-button @click="handle(scope.row.id)" type="text" size="small" v-if="scope.row.batchNO!=undefined">重新上传</el-button>
               </template>
             </el-table-column>
           </el-table>
