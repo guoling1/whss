cf333ceb450c7dd4d95446cd2666990370f76e11
xingliujie <java_xlj@163.com>
2017-04-24 10:21:55 +0800

    绑码

--- common/common-util-spring/src/main/java/com/jkm/base/common/spring/alipay/service/impl/AlipayOauthServiceImpl.java
+++ common/common-util-spring/src/main/java/com/jkm/base/common/spring/alipay/service/impl/AlipayOauthServiceImpl.java
@@ -40,28 +40,28 @@ public class AlipayOauthServiceImpl implements AlipayOauthService {
             AlipaySystemOauthTokenResponse oauthTokenResponse = alipayClient
                     .execute(oauthTokenRequest);
             userId = oauthTokenResponse.getUserId();
-            log.info("成功返回信息为{}",JSONObject.toJSON(oauthTokenResponse).toString());
-            //成功获得authToken
-            if (null != oauthTokenResponse && oauthTokenResponse.isSuccess()) {
-                log.info("利用authToken获取用户信息");
-                AlipayUserUserinfoShareRequest userinfoShareRequest = new AlipayUserUserinfoShareRequest();
-                AlipayUserUserinfoShareResponse userinfoShareResponse = alipayClient.execute(
-                        userinfoShareRequest, oauthTokenResponse.getAccessToken());
-                log.info("利用authToken获取用户信息{}",JSONObject.toJSON(userinfoShareResponse).toString());
-                //成功获得用户信息
-                if (null != userinfoShareResponse && userinfoShareResponse.isSuccess()) {
-                    //这里仅是简单打印， 请开发者按实际情况自行进行处理
-                    System.out.println("获取用户信息成功：" + userinfoShareResponse.getBody());
-
-                } else {
-                    //这里仅是简单打印， 请开发者按实际情况自行进行处理
-                    System.out.println("获取用户信息失败");
-
-                }
-            } else {
-                //这里仅是简单打印， 请开发者按实际情况自行进行处理
-                System.out.println("authCode换取authToken失败");
-            }
+//            log.info("成功返回信息为{}",JSONObject.toJSON(oauthTokenResponse).toString());
+//            //成功获得authToken
+//            if (null != oauthTokenResponse && oauthTokenResponse.isSuccess()) {
+//                log.info("利用authToken获取用户信息");
+//                AlipayUserUserinfoShareRequest userinfoShareRequest = new AlipayUserUserinfoShareRequest();
+//                AlipayUserUserinfoShareResponse userinfoShareResponse = alipayClient.execute(
+//                        userinfoShareRequest, oauthTokenResponse.getAccessToken());
+//                log.info("利用authToken获取用户信息{}",JSONObject.toJSON(userinfoShareResponse).toString());
+//                //成功获得用户信息
+//                if (null != userinfoShareResponse && userinfoShareResponse.isSuccess()) {
+//                    //这里仅是简单打印， 请开发者按实际情况自行进行处理
+//                    System.out.println("获取用户信息成功：" + userinfoShareResponse.getBody());
+//
+//                } else {
+//                    //这里仅是简单打印， 请开发者按实际情况自行进行处理
+//                    System.out.println("获取用户信息失败");
+//
+//                }
+//            } else {
+//                //这里仅是简单打印， 请开发者按实际情况自行进行处理
+//                System.out.println("authCode换取authToken失败");
+//            }
         } catch (AlipayApiException alipayApiException) {
             //自行处理异常
             alipayApiException.printStackTrace();
