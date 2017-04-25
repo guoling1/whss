3e9c555bccb9f1062f3c3318c6d48f110af44914
Administrator <706326182@qq.com>
2017-04-25 10:13:54 +0800

    更改商户认证资料

--- webapp/admin/src/main/java/com/jkm/hss/controller/merchant/PhotoChangeController.java
+++ webapp/admin/src/main/java/com/jkm/hss/controller/merchant/PhotoChangeController.java
@@ -2,6 +2,8 @@ package com.jkm.hss.controller.merchant;
 
 import com.aliyun.oss.OSSClient;
 import com.aliyun.oss.model.ObjectMetadata;
+import com.google.common.base.Preconditions;
+import com.jkm.base.common.entity.BaseEntity;
 import com.jkm.base.common.entity.CommonResponse;
 import com.jkm.base.common.entity.PageModel;
 import com.jkm.hss.controller.BaseController;
@@ -14,15 +16,13 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.IOException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -39,72 +39,99 @@ public class PhotoChangeController extends BaseController {
     @Autowired
     private OSSClient ossClient;
 
+    /**
+     * 判断文件是否是图片
+     * @param file
+     * @return
+     */
+    private static boolean isImage(final MultipartFile file) {
+        List<String> allowType =
+                Arrays.asList("image/png; charset=UTF-8", "image/gif; charset=UTF-8", "image/jpg; charset=UTF-8", "image/jpeg; charset=UTF-8", "image/x-png; charset=UTF-8", "image/pjpeg; charset=UTF-8");
+        return allowType.contains(file.getContentType());
+    }
+
+//    @ResponseBody
+//    @RequestMapping(value = "/savePhotoChang",method = RequestMethod.POST)
+//    public CommonResponse<BaseEntity> savePhotoChang(@RequestParam("photo") MultipartFile file, @RequestBody HistoryPhotoChangeRequest request) {
+//
+//        HistoryPhotoChangeResponse response = merchantInfoQueryService.getPhoto(request.getMerchantId());
+//        request.setOperator(super.getAdminUser().getUsername());
+//
+//            final String photoName = getOrginFileName(file.getOriginalFilename());
+//
+//            Preconditions.checkArgument(!file.isEmpty(), "图片不能为空");
+//            Preconditions.checkArgument(isImage(file), "图片格式不正确");
+//            final ObjectMetadata meta = new ObjectMetadata();
+//            meta.setCacheControl("public, max-age=31536000");
+//            meta.setExpirationTime(new DateTime().plusYears(1).toDate());
+//            meta.setContentType(file.getContentType());
+//        try {
+//            ossClient.putObject(ApplicationConsts.getApplicationConfig().ossBucke(), photoName, file.getInputStream(),meta);
+//            if (request.getType()==1) {
+//                request.setPhoto(response.getBankPic());
+//                merchantInfoQueryService.saveHistory(request);
+//                request.setPhotoName(photoName);
+//                merchantInfoQueryService.savePhotoChang(request);
+//            }
+//            if (request.getType()==2) {
+//                request.setPhoto(response.getBankHandPic());
+//                merchantInfoQueryService.saveHistory(request);
+//                request.setPhotoName(photoName);
+//                merchantInfoQueryService.savePhotoChang1(request);
+//            }
+//            if (request.getType()==3) {
+//                request.setPhoto(response.getIdentityHandPic());
+//                merchantInfoQueryService.saveHistory(request);
+//                request.setPhotoName(photoName);
+//                merchantInfoQueryService.savePhotoChang2(request);
+//            }
+//            if (request.getType()==4) {
+//                request.setPhoto(response.getIdentityFacePic());
+//                merchantInfoQueryService.saveHistory(request);
+//                request.setPhotoName(photoName);
+//                merchantInfoQueryService.savePhotoChang3(request);
+//            }
+//            if (request.getType()==5) {
+//                request.setPhoto(response.getIdentityOppositePic());
+//                merchantInfoQueryService.saveHistory(request);
+//                request.setPhotoName(photoName);
+//                merchantInfoQueryService.savePhotoChang4(request);
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            log.debug("操作异常");
+//        }
+//
+//        return CommonResponse.simpleResponse(1,"操作成功");
+//
+//    }
     @ResponseBody
-    @RequestMapping(value = "/savePhotoChang")
-    public CommonResponse savePhotoChang(@RequestBody HistoryPhotoChangeRequest request) {
-
-        HistoryPhotoChangeResponse response = merchantInfoQueryService.getPhoto(request.getMerchantId());
-        request.setOperator(super.getAdminUser().getUsername());
-        InputStream inputStream = null;
+    @RequestMapping("/savePhotoChang")
+    public CommonResponse<BaseEntity> savePhotoChang(@RequestParam("photo") MultipartFile file,String merchantId) {
+        Preconditions.checkArgument(!file.isEmpty(), "图片不能为空");
+        Preconditions.checkArgument(isImage(file), "图片格式不正确");
+        final String fileName = getOrginFileName(file.getOriginalFilename());
+        final ObjectMetadata meta = new ObjectMetadata();
+        meta.setCacheControl("public, max-age=31536000");
+        meta.setExpirationTime(new DateTime().plusYears(1).toDate());
+        meta.setContentType(file.getContentType());
         try {
-            inputStream = new FileInputStream(request.getPhoto());
-            final ObjectMetadata meta = new ObjectMetadata();
-            meta.setCacheControl("public, max-age=31536000");
-            meta.setExpirationTime(new DateTime().plusYears(1).toDate());
-            meta.setContentType("application/octet-stream ");
-            SimpleDateFormat sdf =   new SimpleDateFormat("yyyyMMdd");
-            String nowDate = sdf.format(new Date());
-            Date date = new Date();
-            long nousedate =  date.getTime();
-            String photoName = "hss/"+  nowDate + "/" + nousedate + RandomStringUtils.randomNumeric(5) +".jpg";
-            ossClient.putObject(ApplicationConsts.getApplicationConfig().ossBucke(), photoName, inputStream,meta);
-            if (request.getType()==1) {
-                request.setPhoto(response.getBankPic());
-                merchantInfoQueryService.saveHistory(request);
-                request.setPhotoName(photoName);
-                merchantInfoQueryService.savePhotoChang(request);
-            }
-            if (request.getType()==2) {
-                request.setPhoto(response.getBankHandPic());
-                merchantInfoQueryService.saveHistory(request);
-                request.setPhotoName(photoName);
-                merchantInfoQueryService.savePhotoChang1(request);
-            }
-            if (request.getType()==3) {
-                request.setPhoto(response.getIdentityHandPic());
-                merchantInfoQueryService.saveHistory(request);
-                request.setPhotoName(photoName);
-                merchantInfoQueryService.savePhotoChang2(request);
-            }
-            if (request.getType()==4) {
-                request.setPhoto(response.getIdentityFacePic());
-                merchantInfoQueryService.saveHistory(request);
-                request.setPhotoName(photoName);
-                merchantInfoQueryService.savePhotoChang3(request);
-            }
-            if (request.getType()==5) {
-                request.setPhoto(response.getIdentityOppositePic());
-                merchantInfoQueryService.saveHistory(request);
-                request.setPhotoName(photoName);
-                merchantInfoQueryService.savePhotoChang4(request);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-
-        }catch (Exception e) {
-            e.printStackTrace();
-            log.debug("操作异常");
+            ossClient.putObject(ApplicationConsts.getApplicationConfig().ossBucke(), fileName, file.getInputStream(), meta);
+        } catch (IOException e) {
+            log.error("上传文件失败", e);
+            CommonResponse.simpleResponse(-1, "图片上传失败");
         }
 
-        return CommonResponse.simpleResponse(1,"操作成功");
-
+        return CommonResponse.builder4MapResult(0, "success")
+                .addParam("url", fileName).build();
     }
+
 
     /**
      * 查看照片
      */
     @ResponseBody
-    @RequestMapping(value = "/selectHistory")
+    @RequestMapping(value = "/selectHistory", method = RequestMethod.POST)
     public CommonResponse selectHistory(@RequestBody HistoryPhotoChangeRequest request) {
         final PageModel<HistoryPhotoChangeResponse> pageModel = new PageModel<HistoryPhotoChangeResponse>(request.getPageNo(), request.getPageSize());
         request.setOffset(pageModel.getFirstIndex());
@@ -124,5 +151,22 @@ public class PhotoChangeController extends BaseController {
         pageModel.setCount(count);
         pageModel.setRecords(list);
         return CommonResponse.objectResponse(1,"success",pageModel);
+    }
+
+
+    /**
+     * 获取随机文件名
+     * @param originalFilename
+     * @return
+     */
+    private String getOrginFileName(final String originalFilename) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        final String extName = originalFilename.substring(originalFilename.lastIndexOf("."));
+        String nowDate = sdf.format(new Date());
+        Date date = new Date();
+        long nousedate = date.getTime();
+        String fileName = "hss/" + nowDate + "/" + nousedate + RandomStringUtils.randomNumeric(5) + extName;
+        return fileName;
+
     }
 }
