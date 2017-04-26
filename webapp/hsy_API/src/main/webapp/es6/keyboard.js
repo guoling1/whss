30c5140d3c957d406a551ff2d009efd594fb2886
于超 <chao.yu@jinkaimen.com>
2017-04-26 16:23:46 +0800

    修改跳转

--- webapp/hsy_API/src/main/webapp/es6/keyboard.js
+++ webapp/hsy_API/src/main/webapp/es6/keyboard.js
@@ -138,9 +138,9 @@ _require.register("keyboard", (module, exports, _require, global) => {
             // 使用以上方式判断前端返回,微信团队郑重提示:res.err_msg 将在用户支付成功后返回ok，但并不保证它绝对可靠。
             function (res) {
               if (res.err_msg == "get_brand_wcpay_request:cancel") {
-                window.location.href = "${pageContext.request.contextPath}/v1/app/qr/showPayResult/" + jsonData.orderNo + "?status=cancel";
+                console.log('取消支付')
               } else if (res.err_msg == "get_brand_wcpay_request:ok") {
-                window.location.href = "${pageContext.request.contextPath}/v1/app/qr/showPayResult/" + jsonData.orderNo;
+                window.location.href = '/trade/success/' + jsonData.orderId;
               } else {
                 alert(res.err_code + res.err_desc + res.err_msg);
               }
