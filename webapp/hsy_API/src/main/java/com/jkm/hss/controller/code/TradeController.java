56e8ba7d537bcb5251bc5caf432aad8181bcff10
yulong.zhang <yulong.zhang@yulongzhangdeMacBook-Pro.local>
2017-04-26 17:11:03 +0800

    `bug`

--- webapp/hsy_API/src/main/java/com/jkm/hss/controller/code/TradeController.java
+++ webapp/hsy_API/src/main/java/com/jkm/hss/controller/code/TradeController.java
@@ -159,7 +159,7 @@ public class TradeController extends BaseController {
             return "/500.jsp";
         }else{
             final Order order = orderOptional.get();
-            model.addAttribute("sn", order.getSn());
+            model.addAttribute("sn", order.getOrderNo());
             model.addAttribute("money", order.getRealPayAmount().toPlainString());
             return "/success";
         }
