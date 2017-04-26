afcc02d692bf0afd8b065a9efd4f58473264f02c
yulong.zhang <yulong.zhang@yulongzhangdeMacBook-Pro.local>
2017-04-26 12:21:42 +0800

    日志

--- webapp/hsy_API/src/main/java/com/jkm/hss/controller/code/TradeController.java
+++ webapp/hsy_API/src/main/java/com/jkm/hss/controller/code/TradeController.java
@@ -60,7 +60,7 @@ public class TradeController extends BaseController {
         final Pair<Integer, String> resultPair = this.hsyTradeService.receipt(payRequest.getTotalFee(),
                 payRequest.getPayChannel(), shop.getId(), EnumAppType.HSY.getId(), payRequest.getMemberId());
         if (0 == resultPair.getLeft()) {
-            return CommonResponse.builder4MapResult(CommonResponse.SUCCESS_CODE, "收款成功")
+            return CommonResponse.builder4MapResult(CommonResponse.SUCCESS_CODE, "success")
                     .addParam("payUrl", URLDecoder.decode(resultPair.getRight(), "UTF-8")).addParam("subMerName", shop.getName())
                     .addParam("amount", payRequest.getTotalFee()).build();
         }
