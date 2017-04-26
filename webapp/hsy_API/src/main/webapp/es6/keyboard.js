dfe6b324027ead037e4559acda09ec6af8b872d2
于超 <chao.yu@jinkaimen.com>
2017-04-26 11:45:20 +0800

    增加弹窗调试

--- webapp/hsy_API/src/main/webapp/es6/keyboard.js
+++ webapp/hsy_API/src/main/webapp/es6/keyboard.js
@@ -151,8 +151,11 @@ _require.register("keyboard", (module, exports, _require, global) => {
         // 唤起支付宝支付
         let onAlipayJSBridge = function (jsonData) {
           //jsonData.channelNo
+          alert(jsonData.channelNo);
+          alert(typeof (jsonData.channelNo));
           AlipayJSBridge.call("tradePay", {tradeNO: jsonData.channelNo},
             function (result) {
+              alert(result);
               let data = JSON.stringify(result);
               if (data.resultCode == 9000 || data.resultCode == 8000) {
                 AlipayJSBridge.call('pushWindow', {
