f71799df3b28c14329e01e3c62ce4b02e01d682d
石永威 <shitou@shiyongweideMacBook-Pro.local>
2017-05-04 16:40:09 +0800

    增加HSY公告列表和详情接口

--- webapp/hsy_API/src/main/java/com/jkm/hss/controller/notice/NoticeController.java
+++ webapp/hsy_API/src/main/java/com/jkm/hss/controller/notice/NoticeController.java
@@ -1,10 +1,13 @@
 package com.jkm.hss.controller.notice;
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
 import com.jkm.base.common.entity.CommonResponse;
 import com.jkm.base.common.entity.PageModel;
 import com.jkm.hss.controller.BaseController;
 import com.jkm.hss.merchant.entity.NoticeRequest;
 import com.jkm.hss.merchant.entity.NoticeResponse;
 import com.jkm.hss.merchant.enums.EnumNotice;
+import com.jkm.hss.merchant.enums.EnumType;
 import com.jkm.hss.merchant.service.PushNoticeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +22,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import com.jkm.hss.helper.response.HsyNoticeResponse;
 
 /**
  * Created by wayne on 17/5/4.
@@ -32,14 +36,16 @@ public class NoticeController extends BaseController {
 
     /**
      * 公告列表
-     * @param request
      * @return
      * @throws ParseException
      */
     @ResponseBody
     @RequestMapping(value = "/noticeList",method = RequestMethod.POST)
-    public CommonResponse noticeList(@RequestBody NoticeRequest request) throws ParseException {
-        final PageModel<NoticeResponse> pageModel = new PageModel<NoticeResponse>(request.getPageNo(), request.getPageSize());
+    public CommonResponse noticeList() throws ParseException {
+        NoticeRequest request=new NoticeRequest();
+        request.setPageNo(1);
+        request.setPageSize(1);
+        final PageModel<HsyNoticeResponse> pageModel = new PageModel<HsyNoticeResponse>(request.getPageNo(), request.getPageSize());
         request.setOffset(pageModel.getFirstIndex());
         if(request.getEndTime()!=null&&!"".equals(request.getEndTime())){
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
@@ -49,6 +55,7 @@ public class NoticeController extends BaseController {
             rightNow.add(Calendar.DATE, 1);
             request.setEndTime(sdf.format(rightNow.getTime()));
         }
+        request.setProductType("hsy");
         List<NoticeResponse> list = pushNoticeService.selectList(request);
         if(list.size()>0){
             for (int i=0;i<list.size();i++){
@@ -57,9 +64,52 @@ public class NoticeController extends BaseController {
                 }
             }
         }
-        int count = pushNoticeService.selectListCount(request);
-        pageModel.setCount(count);
-        pageModel.setRecords(list);
-        return CommonResponse.objectResponse(1, "success", pageModel);
+
+        List<HsyNoticeResponse> hsylist= Lists.transform(list, new Function<NoticeResponse, HsyNoticeResponse>() {
+            @Override
+            public HsyNoticeResponse apply(NoticeResponse noticeResponse) {
+                HsyNoticeResponse hsyNoticeResponse=new HsyNoticeResponse();
+                hsyNoticeResponse.setCreateTime(noticeResponse.getDates());
+                hsyNoticeResponse.setId(noticeResponse.getId());
+                hsyNoticeResponse.setStatus(noticeResponse.getStatus());
+                hsyNoticeResponse.setPushStatus(noticeResponse.getPushStatus());
+                hsyNoticeResponse.setTitle(noticeResponse.getTitle());
+                hsyNoticeResponse.setUrl("http://hsy.qianbaojiajia.com/"+noticeResponse.getId());
+                return hsyNoticeResponse;
+            }
+        });
+        return CommonResponse.objectResponse(1, "success", hsylist);
     }
+
+    /**
+     * 公告详情
+     * @param request
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "/noticeDetails",method = RequestMethod.POST)
+    public CommonResponse noticeDetails(@RequestBody NoticeRequest request){
+        try{
+            final NoticeResponse result = this.pushNoticeService.noticeDetails(request.getId());
+            if(result!=null) {
+                if (result.getType().equals("1")) {
+                    result.setType(EnumType.MAINTAIN.getValue());
+                }
+                if (result.getType().equals("2")) {
+                    result.setType(EnumType.NOTICE.getValue());
+                }
+                if(result.getStatus()==1){
+                    result.setPushStatus(EnumNotice.PUBLISHED.getValue());
+                }
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                String dates = sdf.format(result.getCreateTime());
+                result.setDates(dates);
+            }
+            return  CommonResponse.objectResponse(1, "success", result);
+        }catch (final Throwable throwable){
+            log.error("获取详情信息异常:" + throwable.getMessage());
+        }
+        return CommonResponse.simpleResponse(-1, "fail");
+    }
+
 }
