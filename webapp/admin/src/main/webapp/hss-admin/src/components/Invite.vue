1eb4103b58a3d7dc535db5c717293f35830a6f41
guoling1 <646626114@qq.com>
2017-05-05 16:00:49 +0800

    页面

--- webapp/admin/src/main/webapp/hss-admin/src/components/Invite.vue
+++ webapp/admin/src/main/webapp/hss-admin/src/components/Invite.vue
@@ -10,10 +10,12 @@
             <div class="form-group">
               <div class="col-xs-12">
                 产品选择：
-                <select class="form-control select2 select2-hidden-accessible" style="width: 25%;display: inline-block" tabindex="-1" aria-hidden="true" v-model="productId">
-                  <option value="">请选择产品</option>
-                  <option :value="list.productId" v-for="list in lists">{{list.productName}}</option>
-                </select>
+                <el-select v-model="productId" placeholder="请选择" size="small">
+                  <el-option v-for="list in lists" :key="list.productId"
+                    :label="list.productName"
+                    :value="list.productId">
+                  </el-option>
+                </el-select>
                 <div @click="search" class="btn btn-primary">确定</div>
               </div>
             </div>
@@ -27,26 +29,18 @@
         <form class="form-horizontal">
           <div class="box-body">
             <div class="form-group">
-              <div class="col-xs-12">
-                <el-table :data="tableData1" style="width: 100%;font-size: 12px">
-                  <el-table-column prop="name" label="合伙人等级参与的通道" width="180"></el-table-column>
-                  <el-table-column prop="data1" label="默认分润空间" width="180"></el-table-column>
-                  <el-table-column prop="data2" label="普通（基准费率）"></el-table-column>
-                  <el-table-column prop="data3" label="店员"></el-table-column>
-                  <el-table-column prop="data4" label="店长"></el-table-column>
-                  <el-table-column prop="data5" label="老板"></el-table-column>
-                </el-table>
+              <div class="col-xs-10">
                 <div class="box box1">
                   <div class="box-body table-responsive no-padding">
                     <table class="table table-hover">
                       <tbody>
                       <tr>
                         <th>合伙人等级<br/>参与的通道</th>
-                        <th>默认分润空间</th>
-                        <th>普通（基准费率）</th>
-                        <th>店员</th>
-                        <th>店长</th>
-                        <th>老板</th>
+                        <th style="line-height: 250%">默认分润空间</th>
+                        <th style="line-height: 250%">普通（基准费率）</th>
+                        <th style="line-height: 250%">店员</th>
+                        <th style="line-height: 250%">店长</th>
+                        <th style="line-height: 250%">老板</th>
                       </tr>
                       <!--<tr>
                         <td>普通</td>
@@ -69,7 +63,7 @@
                         <td><input type="text" name="name" v-model="upgrade.inDirectPromoteShall">元</td>
                       </tr>-->
                       <tr >
-                        <td>点击添加通道</td>
+                        <td><el-button type="text" size="small" @click="isAdd = true">点击添加通道</el-button></td>
                         <td></td>
                         <td></td>
                         <td></td>
@@ -78,57 +72,83 @@
                       </tr>
                       </tbody></table>
                   </div>
-                  <!-- /.box-body -->
+                  <el-dialog title="添加通道" v-model="isAdd">
+                    <el-form :label-position="right" label-width="150px">
+                      <el-form-item label="选择通道：" width="120" style="margin-bottom: 0">
+                        <el-select size="small" placeholder="请选择" v-model="addQuery.a">
+                          <el-option label="切换金开门直属" value="1"></el-option>
+                          <el-option label="切换为一级直属" value="2"></el-option>
+                          <el-option label="切换到二级" value="3"></el-option>
+                        </el-select>
+                      </el-form-item>
+                      <el-form-item label="默认分润空间：" width="120" style="margin-bottom: 0">
+                        <el-input style="width: 70%" size="small" v-model="addQuery.b"></el-input>
+                      </el-form-item>
+                    </el-form>
+                    <div slot="footer" class="dialog-footer" style="text-align: center">
+                      <el-button @click="isAdd = false" style="position: relative;top: -20px;">取 消</el-button>
+                      <el-button @click="add" type="primary" style="position: relative;top: -20px;">确 定</el-button>
+                    </div>
+                  </el-dialog>
+                  <el-dialog title="确认添加" v-model="isSub">
+                    <el-form :label-position="right" label-width="150px">
+                      <el-form-item label="通道名称：" width="120" style="margin-bottom: 0">
+                        <span>xx</span>
+                      </el-form-item>
+                      <el-form-item label="支付方式：" width="120" style="margin-bottom: 0">
+                        <span>xx</span>
+                      </el-form-item>
+                    </el-form>
+                    <div slot="footer" class="dialog-footer" style="text-align: center">
+                      <el-button @click="isSub = false" style="position: relative;top: -20px;">取 消</el-button>
+                      <el-button @click="isSub = false" type="primary" style="position: relative;top: -20px;">确 定</el-button>
+                    </div>
+                  </el-dialog>
                 </div>
               </div>
-            </div>
-          </div>
-        </form>
-      </div>
-      <div class="box box-info" v-if="result.length!=0">
-        <div class="box-header with-border">
-          <h3 class="box-title">商户升级规则设置</h3>
-        </div>
-        <form class="form-horizontal">
-          <div class="box-body">
-            <div class="form-group">
-              <div class="col-xs-12">
+              <div class="col-xs-8">
                 <div class="box box1">
                   <div class="box-body table-responsive no-padding">
                     <table class="table table-hover">
                       <tbody>
                       <tr>
-                        <th>合伙人等级</th>
-                        <th>微信费率</th>
-                        <th>支付宝费率</th>
-                        <th>无卡费率</th>
-                        <th>升级费</th>
-                        <th>邀请人数</th>
-                        <th>直接升级费奖励</th>
-                        <th>间推升级费奖励</th>
+                        <th>合伙人等级<br/>通道名称</th>
+                        <th style="line-height: 250%">普通</th>
+                        <th style="line-height: 250%">店员</th>
+                        <th style="line-height: 250%">店长</th>
+                        <th style="line-height: 250%">老板</th>
                       </tr>
                       <tr>
-                        <td>普通</td>
-                        <td>{{result.upgradeRulesList[0].weixinRate}}%</td>
-                        <td>{{result.upgradeRulesList[0].alipayRate}}%</td>
-                        <td>{{result.upgradeRulesList[0].fastRate}}%</td>
+                        <td>升级费</td>
                         <td>无</td>
-                        <td>无</td>
-                        <td>无</td>
-                        <td>无</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
                       </tr>
-                      <tr v-for="(upgrade,index) in result.upgradeRulesList" v-if="index!=0">
-                        <td>{{upgrade.name}}</td>
-                        <td><input type="text" name="name" v-model="upgrade.weixinRate">%</td>
-                        <td><input type="text" name="name" v-model="upgrade.alipayRate">%</td>
-                        <td><input type="text" name="name" v-model="upgrade.fastRate">%</td>
-                        <td><input type="text" name="name" v-model="upgrade.upgradeCost">元</td>
-                        <td><input type="text" name="name" v-model="upgrade.promotionNum">人</td>
-                        <td><input type="text" name="name" v-model="upgrade.directPromoteShall">元</td>
-                        <td><input type="text" name="name" v-model="upgrade.inDirectPromoteShall">元</td>
+                      <tr>
+                        <td>直推升级费奖励</td>
+                        <td>无</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td>间推升级费奖励</td>
+                        <td>无</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td>邀请升级达标人数</td>
+                        <td>无</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
                       </tr>
                       <tr >
-                        <td colspan="6">有效激活标准：收款满<input type="text" name="name" style="width: 100px;" v-model="result.standard">元
+                        <td>邀请用户有效标准</td>
+                        <td colspan="4">有效激活标准：收款满<input type="text" name="name" style="width: 100px;" v-model="result.standard">元
                         </td>
                       </tr>
                       </tbody></table>
@@ -141,7 +161,7 @@
           </div>
         </form>
       </div>
-      <div class="box box-info" v-if="result.length!=0">
+      <div class="box box-info">
         <div class="box-header with-border">
           <h3 class="box-title">升级推荐分润设置</h3>
         </div>
@@ -165,10 +185,6 @@
                         <td>收单分润</td>
                         <td><input type="text" name="name" v-model="result.tradeRate">%</td>
                       </tr>
-                      <!--<tr >
-                        <td colspan="2">收单奖励分润池：<input type="text" name="name" style="width: 100px;" v-model="result.rewardRate">%
-                        </td>
-                      </tr>-->
                       </tbody>
                     </table>
                   </div>
@@ -178,7 +194,7 @@
           </div>
         </form>
       </div>
-      <div class="btn btn-primary" @click="save" v-if="result.length!=0">保 存</div>
+      <div class="btn btn-primary" @click="save">保 存</div>
       <span v-if="result.length!=0">(对新审核通过的有效)</span>
     </div>
   </div>
@@ -189,6 +205,12 @@
     name:"invite",
     data(){
       return{
+        isAdd:false,
+        isSub:false,
+        addQuery:{
+           a:'',
+          b:''
+        },
         tableData1:[{
           name:'微信',
           data1:'',
@@ -236,11 +258,14 @@
         })*/
     },
     methods: {
+      add:function () {
+        this.isAdd = false;
+        this.isSub = true;
+      },
       search: function () {
         this.$data.result = ''
         this.$http.post('/admin/upgrade/init', {productId: this.$data.productId})
           .then(function (res) {
-            console.log(res)
             this.$data.result = res.data;
             this.$data.result.upgradeRulesList.sort(function (a, b) {
               return a.type-b.type
@@ -301,10 +326,7 @@
     padding: 0;
   }
 
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
+
   input {
     width: 77%;
     border: none;
