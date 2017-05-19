cbc174117a362d2cedda4c2c059c79e8b68bb4c3
guoling1 <646626114@qq.com>
2017-05-19 15:16:40 +0800

    订单支付方式修改

--- webapp/admin/src/main/webapp/hss-admin/src/components/OrderQuery.vue
+++ webapp/admin/src/main/webapp/hss-admin/src/components/OrderQuery.vue
@@ -44,10 +44,10 @@
                   <label>支付方式：</label>
                   <el-select style="width: 188px" clearable v-model="queryHss.paymentMethod" size="small">
                     <el-option label="全部" value="">全部</el-option>
-                    <el-option label="微信支付" value="wechat"></el-option>
-                    <el-option label="支付宝支付" value="alipay"></el-option>
-                    <el-option label="快捷支付" value="unionpay"></el-option>
-                    <el-option label="QQ钱包" value="qqpay"></el-option>
+                    <el-option label="微信支付" value="1"></el-option>
+                    <el-option label="支付宝支付" value="2"></el-option>
+                    <el-option label="快捷支付" value="3"></el-option>
+                    <el-option label="QQ钱包" value="4"></el-option>
                   </el-select>
                 </li>
                 <li class="same">
