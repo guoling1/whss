e68e23fba7242e6c478b69d657ed8ed46f88fbdf
于超 <chao.yu@jinkaimen.com>
2017-05-23 14:12:23 +0800

    接口调试

--- webapp/dealer_PC/src/main/webapp/dealer-pc/src/components/qrcode.vue
+++ webapp/dealer_PC/src/main/webapp/dealer-pc/src/components/qrcode.vue
@@ -27,6 +27,10 @@
                 <el-input v-model="merchantName" placeholder="商户名称" size="small" style="width:240px"></el-input>
               </div>
               <div class="screen-item">
+                <span class="screen-title">一级代理名称</span>
+                <el-input v-model="firstDealerName" placeholder="一级代理名称" size="small" style="width:240px"></el-input>
+              </div>
+              <div class="screen-item">
                 <span class="screen-title">二级代理名称</span>
                 <el-input v-model="secondDealerName" placeholder="二级代理名称" size="small" style="width:240px"></el-input>
               </div>
@@ -130,6 +134,7 @@
         tableLoading: false,
         code: '',
         merchantName: '',
+        firstDealerName: '',
         secondDealerName: '',
         businessType: 'hss',
         item_businessType: [
@@ -199,6 +204,7 @@
           pageNo: this.pageNo,
           code: this.code,
           merchantName: this.merchantName,
+          firstDealerName: this.firstDealerName,
           secondDealerName: this.secondDealerName,
           sysType: this.businessType,
           activateStatus: this.activeType,
