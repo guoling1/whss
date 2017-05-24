0cef9abcfbf5ed671924b1903743e638be7f02ca
于超 <chao.yu@jinkaimen.com>
2017-05-24 14:18:36 +0800

    调试接口

--- webapp/dealer_PC/src/main/webapp/dealer-pc/src/components/policy.vue
+++ webapp/dealer_PC/src/main/webapp/dealer-pc/src/components/policy.vue
@@ -2,266 +2,153 @@
   <div class="content-wrapper">
     <!-- Main content -->
     <section class="content">
-      <h4 class="text-center top">
-        您好，{{dealerInfo}}
-      </h4>
-      <h2 class="text-center bottom">
-        代理政策 正在开发中,敬请期待
-      </h2>
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="box">
+            <div class="box-header">
+              <h3 class="box-title">代理政策</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <el-tabs v-model="activeName2" type="card" @tab-click="handleClick">
+                <el-tab-pane label="好收收" name="first">好收收</el-tab-pane>
+                <el-tab-pane label="好收银" name="second">好收银</el-tab-pane>
+              </el-tabs>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
     </section>
   </div>
 </template>
 <script lang="babel">
-  import store from '../store'
   export default {
-    data () {
+    name: 'app',
+    data() {
       return {
-        dealerInfo: ''
+        activeName: 'first',
+        total: 0,
+        pageSize: 20,
+        pageNo: 1,
+        tableData: [],
+        tableLoading: false,
+        orderNo: '',
+        businessType: '',
+        item_businessType: [
+          {
+            value: 'hssPay',
+            label: '好收收-收款'
+          },
+          {
+            value: 'hssWithdraw',
+            label: '好收收-提现'
+          },
+          {
+            value: 'hssPromote',
+            label: '好收收-升级费'
+          },
+          {
+            value: 'hsyPay',
+            label: '好收银-收款'
+          }
+        ],
+        datetime: '',
+        beginDate: '',
+        endDate: '',
+        pickerOptions2: {
+          shortcuts: [{
+            text: '最近一周',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+              picker.$emit('pick', [start, end]);
+            }
+          }, {
+            text: '最近一个月',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+              picker.$emit('pick', [start, end]);
+            }
+          }, {
+            text: '最近三个月',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+              picker.$emit('pick', [start, end]);
+            }
+          }]
+        }
       }
     },
-    mounted (){
-      this.$store.dispatch('actions_users_getInfo').then(data => {
-        if (data.status === 1) {
-          this.dealerInfo = data.dealerInfo;
-        }
-      });
+    created() {
+//this.getData();
     },
-    methods: {}
+    methods: {
+      handleClick(tab, event) {
+        console.log(tab, event);
+      },
+      datetimeSelect: function (val) {
+        let format = val.split(' - ');
+        this.beginDate = format[0];
+        this.endDate = format[1];
+      },
+      screen: function () {
+        this.getData();
+      },
+      getData: function () {
+        this.tableLoading = true;
+        this.$http.post('/daili/profit/details', {
+          pageSize: this.pageSize,
+          pageNo: this.pageNo,
+          orderNo: this.orderNo,
+          businessType: this.businessType,
+          beginDate: this.beginDate,
+          endDate: this.endDate
+        }).then(res => {
+          this.tableLoading = false;
+          this.total = res.data.count;
+          this.tableData = res.data.records;
+        }, err => {
+          this.tableLoading = false;
+          this.$message({
+            showClose: true,
+            message: err.data.msg,
+            type: 'error'
+          });
+        });
+      },
+      handleSizeChange(val) {
+        this.pageSize = val;
+        this.getData();
+      },
+      handleCurrentChange(val) {
+        this.pageNo = val;
+        this.getData();
+      }
+    }
   }
 </script>
 <style scoped lang="less">
-  .top {
-    margin-top: 200px;
+  .screen-top {
+    padding-top: 0 !important;
   }
 
-  .bottom {
-    margin-bottom: 200px;
+  .screen-item {
+    float: left;
+    margin-right: 10px;
+  }
+
+  .screen-title {
+    display: block;
+    height: 24px;
+    line-height: 24px;
   }
 </style>
-
-<!--<template lang="html">-->
-<!--<div class="content-wrapper">-->
-<!--&lt;!&ndash; Main content &ndash;&gt;-->
-<!--<section class="content">-->
-<!--<div class="row">-->
-<!--<div class="col-xs-12">-->
-<!--<div class="box">-->
-<!--<div class="box-header">-->
-<!--<h3 class="box-title">代理政策</h3>-->
-<!--</div>-->
-<!--&lt;!&ndash; /.box-header &ndash;&gt;-->
-<!--<div class="box-body">-->
-<!--<el-tabs v-model="activeName2" type="card" @tab-click="handleClick">-->
-<!--<el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>-->
-<!--<el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>-->
-<!--<el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>-->
-<!--<el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>-->
-<!--</el-tabs>-->
-<!--</div>-->
-
-<!--<div class="box-body screen-top">-->
-<!--<div class="screen-item">-->
-<!--<span class="screen-title">交易订单号</span>-->
-<!--<el-input v-model="orderNo" placeholder="交易订单号" size="small" style="width:240px"></el-input>-->
-<!--</div>-->
-<!--<div class="screen-item">-->
-<!--<span class="screen-title">业务类型</span>-->
-<!--<el-select v-model="businessType" size="small" clearable placeholder="请选择">-->
-<!--<el-option v-for="item in item_businessType"-->
-<!--:label="item.label"-->
-<!--:value="item.value">-->
-<!--</el-option>-->
-<!--</el-select>-->
-<!--</div>-->
-<!--<div class="screen-item">-->
-<!--<span class="screen-title">分润日期</span>-->
-<!--<el-date-picker size="small"-->
-<!--v-model="datetime"-->
-<!--type="daterange"-->
-<!--align="right"-->
-<!--@change="datetimeSelect"-->
-<!--placeholder="选择日期范围"-->
-<!--:picker-options="pickerOptions2">-->
-<!--</el-date-picker>-->
-<!--</div>-->
-<!--<div class="screen-item">-->
-<!--<span class="screen-title"></span>-->
-<!--<el-button type="primary" size="small" @click="screen">筛选</el-button>-->
-<!--</div>-->
-<!--</div>-->
-<!--<div class="box-body">-->
-<!--<el-table :data="tableData" border-->
-<!--v-loading="tableLoading"-->
-<!--element-loading-text="数据加载中">-->
-<!--<el-table-column type="index" label="序号"></el-table-column>-->
-<!--<el-table-column prop="splitOrderNo" label="分润流水号"></el-table-column>-->
-<!--<el-table-column label="业务类型">-->
-<!--<template scope="scope">-->
-<!--{{ scope.row.businessType | filter_businessType }}-->
-<!--</template>-->
-<!--</el-table-column>-->
-<!--<el-table-column label="分润时间" width="180">-->
-<!--<template scope="scope">-->
-<!--{{ scope.row.splitCreateTime | datetime }}-->
-<!--</template>-->
-<!--</el-table-column>-->
-<!--<el-table-column prop="orderNo" label="交易订单号"></el-table-column>-->
-<!--<el-table-column prop="splitSettlePeriod" label="分润结算周期"></el-table-column>-->
-<!--<el-table-column label="结算时间">-->
-<!--<template scope="scope">-->
-<!--{{ scope.row.settleTime | datetime }}-->
-<!--</template>-->
-<!--</el-table-column>-->
-<!--<el-table-column prop="dealerName" label="代理商名称"></el-table-column>-->
-<!--<el-table-column prop="splitAmount" label="分润金额" align="right"></el-table-column>-->
-<!--<el-table-column prop="remark" label="备注信息"></el-table-column>-->
-<!--</el-table>-->
-<!--</div>-->
-<!--<div class="box-body">-->
-<!--<el-pagination style="float:right"-->
-<!--@size-change="handleSizeChange"-->
-<!--@current-change="handleCurrentChange"-->
-<!--:current-page="pageNo"-->
-<!--:page-sizes="[20, 100, 200, 500]"-->
-<!--:page-size="pageSize"-->
-<!--layout="total, sizes, prev, pager, next, jumper"-->
-<!--:total="total">-->
-<!--</el-pagination>-->
-<!--</div>-->
-<!--&lt;!&ndash; /.box-body &ndash;&gt;-->
-<!--</div>-->
-<!--&lt;!&ndash; /.box &ndash;&gt;-->
-<!--</div>-->
-<!--&lt;!&ndash; /.col &ndash;&gt;-->
-<!--</div>-->
-<!--&lt;!&ndash; /.row &ndash;&gt;-->
-<!--</section>-->
-<!--</div>-->
-<!--</template>-->
-<!--<script lang="babel">-->
-<!--export default {-->
-<!--name: 'app',-->
-<!--data() {-->
-<!--return {-->
-<!--activeName: 'first',-->
-<!--total: 0,-->
-<!--pageSize: 20,-->
-<!--pageNo: 1,-->
-<!--tableData: [],-->
-<!--tableLoading: false,-->
-<!--orderNo: '',-->
-<!--businessType: '',-->
-<!--item_businessType: [-->
-<!--{-->
-<!--value: 'hssPay',-->
-<!--label: '好收收-收款'-->
-<!--},-->
-<!--{-->
-<!--value: 'hssWithdraw',-->
-<!--label: '好收收-提现'-->
-<!--},-->
-<!--{-->
-<!--value: 'hssPromote',-->
-<!--label: '好收收-升级费'-->
-<!--},-->
-<!--{-->
-<!--value: 'hsyPay',-->
-<!--label: '好收银-收款'-->
-<!--}-->
-<!--],-->
-<!--datetime: '',-->
-<!--beginDate: '',-->
-<!--endDate: '',-->
-<!--pickerOptions2: {-->
-<!--shortcuts: [{-->
-<!--text: '最近一周',-->
-<!--onClick(picker) {-->
-<!--const end = new Date();-->
-<!--const start = new Date();-->
-<!--start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);-->
-<!--picker.$emit('pick', [start, end]);-->
-<!--}-->
-<!--}, {-->
-<!--text: '最近一个月',-->
-<!--onClick(picker) {-->
-<!--const end = new Date();-->
-<!--const start = new Date();-->
-<!--start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);-->
-<!--picker.$emit('pick', [start, end]);-->
-<!--}-->
-<!--}, {-->
-<!--text: '最近三个月',-->
-<!--onClick(picker) {-->
-<!--const end = new Date();-->
-<!--const start = new Date();-->
-<!--start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);-->
-<!--picker.$emit('pick', [start, end]);-->
-<!--}-->
-<!--}]-->
-<!--}-->
-<!--}-->
-<!--},-->
-<!--created() {-->
-<!--//this.getData();-->
-<!--},-->
-<!--methods: {-->
-<!--handleClick(tab, event) {-->
-<!--console.log(tab, event);-->
-<!--},-->
-<!--datetimeSelect: function (val) {-->
-<!--let format = val.split(' - ');-->
-<!--this.beginDate = format[0];-->
-<!--this.endDate = format[1];-->
-<!--},-->
-<!--screen: function () {-->
-<!--this.getData();-->
-<!--},-->
-<!--getData: function () {-->
-<!--this.tableLoading = true;-->
-<!--this.$http.post('/daili/profit/details', {-->
-<!--pageSize: this.pageSize,-->
-<!--pageNo: this.pageNo,-->
-<!--orderNo: this.orderNo,-->
-<!--businessType: this.businessType,-->
-<!--beginDate: this.beginDate,-->
-<!--endDate: this.endDate-->
-<!--}).then(res => {-->
-<!--this.tableLoading = false;-->
-<!--this.total = res.data.count;-->
-<!--this.tableData = res.data.records;-->
-<!--}, err => {-->
-<!--this.tableLoading = false;-->
-<!--this.$message({-->
-<!--showClose: true,-->
-<!--message: err.data.msg,-->
-<!--type: 'error'-->
-<!--});-->
-<!--});-->
-<!--},-->
-<!--handleSizeChange(val) {-->
-<!--this.pageSize = val;-->
-<!--this.getData();-->
-<!--},-->
-<!--handleCurrentChange(val) {-->
-<!--this.pageNo = val;-->
-<!--this.getData();-->
-<!--}-->
-<!--}-->
-<!--}-->
-<!--</script>-->
-<!--<style scoped lang="less">-->
-<!--.screen-top {-->
-<!--padding-top: 0 !important;-->
-<!--}-->
-
-<!--.screen-item {-->
-<!--float: left;-->
-<!--margin-right: 10px;-->
-<!--}-->
-
-<!--.screen-title {-->
-<!--display: block;-->
-<!--height: 24px;-->
-<!--line-height: 24px;-->
-<!--}-->
-<!--</style>-->
