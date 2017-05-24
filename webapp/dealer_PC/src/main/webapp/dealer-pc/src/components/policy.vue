f146bf9ae402dd9993f21f43bc63b66b28953aeb
于超 <chao.yu@jinkaimen.com>
2017-05-24 15:56:30 +0800

    接口调试

--- webapp/dealer_PC/src/main/webapp/dealer-pc/src/components/policy.vue
+++ webapp/dealer_PC/src/main/webapp/dealer-pc/src/components/policy.vue
@@ -11,7 +11,26 @@
             <!-- /.box-header -->
             <div class="box-body">
               <el-tabs v-model="activeName2" type="card" @tab-click="handleClick">
-                <el-tab-pane label="好收收" name="first">好收收</el-tab-pane>
+                <el-tab-pane label="好收收" name="first">
+                  <div class="box-header with-border">
+                    <h3 class="box-title">收款及提现结算价</h3>
+                  </div>
+                  <div class="box-body">
+                    <el-table :data="tableData" border
+                              v-loading="tableLoading"
+                              element-loading-text="数据加载中">
+                      <el-table-column label="注册时间" width="180">
+                        <template scope="scope">
+                          {{ scope.row.createTime | datetime }}
+                        </template>
+                      </el-table-column>
+                      <el-table-column prop="mobile" label="联系手机号"></el-table-column>
+                    </el-table>
+                  </div>
+                  <div class="box-header with-border">
+                    <h3 class="box-title">合伙人推荐分润设置</h3>
+                  </div>
+                </el-tab-pane>
                 <el-tab-pane label="好收银" name="second">好收银</el-tab-pane>
               </el-tabs>
             </div>
@@ -29,93 +48,18 @@
   export default {
     name: 'app',
     data() {
-      return {
-        activeName: 'first',
-        total: 0,
-        pageSize: 20,
-        pageNo: 1,
-        tableData: [],
-        tableLoading: false,
-        orderNo: '',
-        businessType: '',
-        item_businessType: [
-          {
-            value: 'hssPay',
-            label: '好收收-收款'
-          },
-          {
-            value: 'hssWithdraw',
-            label: '好收收-提现'
-          },
-          {
-            value: 'hssPromote',
-            label: '好收收-升级费'
-          },
-          {
-            value: 'hsyPay',
-            label: '好收银-收款'
-          }
-        ],
-        datetime: '',
-        beginDate: '',
-        endDate: '',
-        pickerOptions2: {
-          shortcuts: [{
-            text: '最近一周',
-            onClick(picker) {
-              const end = new Date();
-              const start = new Date();
-              start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
-              picker.$emit('pick', [start, end]);
-            }
-          }, {
-            text: '最近一个月',
-            onClick(picker) {
-              const end = new Date();
-              const start = new Date();
-              start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
-              picker.$emit('pick', [start, end]);
-            }
-          }, {
-            text: '最近三个月',
-            onClick(picker) {
-              const end = new Date();
-              const start = new Date();
-              start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
-              picker.$emit('pick', [start, end]);
-            }
-          }]
-        }
-      }
+      return {}
     },
     created() {
-//this.getData();
+      this.getData();
     },
     methods: {
-      handleClick(tab, event) {
-        console.log(tab, event);
-      },
-      datetimeSelect: function (val) {
-        let format = val.split(' - ');
-        this.beginDate = format[0];
-        this.endDate = format[1];
-      },
-      screen: function () {
-        this.getData();
-      },
       getData: function () {
         this.tableLoading = true;
-        this.$http.post('/daili/profit/details', {
-          pageSize: this.pageSize,
-          pageNo: this.pageNo,
-          orderNo: this.orderNo,
-          businessType: this.businessType,
-          beginDate: this.beginDate,
-          endDate: this.endDate
+        this.$http.post('/daili/dealer/dealerPolicy', {
+          sysType: 'hss'
         }).then(res => {
-          this.tableLoading = false;
-          this.total = res.data.count;
-          this.tableData = res.data.records;
+          console.log(res);
         }, err => {
           this.tableLoading = false;
           this.$message({
@@ -124,14 +68,6 @@
             type: 'error'
           });
         });
-      },
-      handleSizeChange(val) {
-        this.pageSize = val;
-        this.getData();
-      },
-      handleCurrentChange(val) {
-        this.pageNo = val;
-        this.getData();
       }
     }
   }
