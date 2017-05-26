f0180cb5393622b931ae88743a0a42658edd3f0d
石永威 <syw0322@163.com>
2017-05-26 10:38:25 +0800

    会员卡EnumPayChannelSign改成1101

--- service/product/product-api/src/main/java/com/jkm/hss/product/enums/EnumPayChannelSign.java
+++ service/product/product-api/src/main/java/com/jkm/hss/product/enums/EnumPayChannelSign.java
@@ -23,7 +23,7 @@ public enum                EnumPayChannelSign {
     /**
      * 会员卡支付
      */
-    MEMBER(901, "member_card", EnumPaymentChannel.MEMBER_PAY, "会员卡", EnumUpperChannel.MEMBER, false, EnumBalanceTimeType.T1, false),
+    MEMBER(1101, "member_card", EnumPaymentChannel.MEMBER_PAY, "会员卡", EnumUpperChannel.MEMBER, false, EnumBalanceTimeType.T1, false),
 
     /**
      * 阳光微信
