27cd4e8edd3ce5af97e7bf34c8b6ed10a51a29ef
yulong.zhang <yulong.zhang@yulongzhangdeMacBook-Pro.local>
2017-05-27 14:54:08 +0800

    修改支付成功，先推送，在入账，发消息推送

--- webapp/admin/src/main/java/com/jkm/hss/mq/consumer/MessageListenerImpl.java
+++ webapp/admin/src/main/java/com/jkm/hss/mq/consumer/MessageListenerImpl.java
@@ -5,7 +5,6 @@ import com.aliyun.openservices.ons.api.Action;
 import com.aliyun.openservices.ons.api.ConsumeContext;
 import com.aliyun.openservices.ons.api.Message;
 import com.aliyun.openservices.ons.api.MessageListener;
-import com.jkm.hss.bill.service.HSYTradeService;
 import com.jkm.hss.mq.config.MqConfig;
 import com.jkm.hss.settle.service.AccountSettleAuditRecordService;
 import lombok.extern.slf4j.Slf4j;
@@ -22,9 +21,6 @@ public class MessageListenerImpl implements MessageListener {
     @Qualifier("accountSettleAuditRecordService")
     private AccountSettleAuditRecordService accountSettleAuditRecordService;
 
-    @Autowired
-    @Qualifier("hsyTradeService")
-    private HSYTradeService hsyTradeService;
     /**
      * 消费消息
      *
@@ -43,11 +39,6 @@ public class MessageListenerImpl implements MessageListener {
                 log.info("消费消息--结算审核记录[{}]， 结算", body.getLong("recordId"));
                 final long recordId = body.getLong("recordId");
                 this.accountSettleAuditRecordService.settleImpl(recordId);
-            } else if (MqConfig.SPLIT_PROFIT.equals(message.getTag())) {
-                log.info("消费消息--支付分润[{}]", body.getLong("orderId"));
-                final long orderId = body.getLong("orderId");
-                final long accountId = body.getLong("accountId");
-                this.hsyTradeService.paySplitAccount(orderId, accountId);
             }
         } catch (Throwable e) {
             log.info("consume message error, Topic is: [{}], tag is: [{}] MsgId is: [{}]", message.getTopic(),
--- webapp/hsy_API/src/main/java/com/jkm/hss/mq/consumer/MessageListenerImpl.java
+++ webapp/hsy_API/src/main/java/com/jkm/hss/mq/consumer/MessageListenerImpl.java
@@ -1,9 +1,12 @@
 package com.jkm.hss.mq.consumer;
 
+import com.alibaba.fastjson.JSONObject;
 import com.aliyun.openservices.ons.api.Action;
 import com.aliyun.openservices.ons.api.ConsumeContext;
 import com.aliyun.openservices.ons.api.Message;
 import com.aliyun.openservices.ons.api.MessageListener;
+import com.jkm.hss.bill.service.HSYTradeService;
+import com.jkm.hss.mq.config.MqConfig;
 import com.jkm.hss.settle.service.AccountSettleAuditRecordService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +19,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 public class MessageListenerImpl implements MessageListener {
 
     @Autowired
-    @Qualifier("accountSettleAuditRecordService")
-    private AccountSettleAuditRecordService accountSettleAuditRecordService;
+    @Qualifier("hsyTradeService")
+    private HSYTradeService hsyTradeService;
     /**
      * 消费消息
      *
@@ -30,6 +33,13 @@ public class MessageListenerImpl implements MessageListener {
         log.info("Receive message, Topic is: [{}], tag is: [{}] MsgId is: [{}]", message.getTopic(),
                 message.getTag(), message.getMsgID());
         try {
+            final JSONObject body = JSONObject.parseObject(new String(message.getBody(),"UTF-8"));
+            if (MqConfig.SPLIT_PROFIT.equals(message.getTag())) {
+                log.info("消费消息--支付分润[{}]", body.getLong("orderId"));
+                final long orderId = body.getLong("orderId");
+                final long accountId = body.getLong("accountId");
+                this.hsyTradeService.paySplitAccount(orderId, accountId);
+            }
 
         } catch (Throwable e) {
             log.info("consume message error, Topic is: [{}], tag is: [{}] MsgId is: [{}] key is [{}]", message.getTopic(),
