f17646ce631e6816614fea6b7e534e2b9ee9585c
于超 <chao.yu@jinkaimen.com>
2017-06-06 19:05:02 +0800

    修改bug

--- webapp/hss_H5/src/main/webapp/es6/hss/notisDetails.js
+++ webapp/hss_H5/src/main/webapp/es6/hss/notisDetails.js
@@ -11,13 +11,35 @@ const browser = _require('browser');
 browser.elastic_touch('noticeDetails');
 // 定义ajax事件
 let query = tools.GetUrlArg();
-let title =document.getElementById('title');
-let date =document.getElementById('date');
-let context =document.getElementById('context');
+let title = document.getElementById('title');
+let date = document.getElementById('date');
+let context = document.getElementById('context');
+
+let changeTitle = function (title) {
+  let body = document.getElementsByTagName('body')[0];
+  if (!title) {
+    document.title = '好收收';
+  } else {
+    document.title = title;
+  }
+  let iframe = document.createElement("iframe");
+  iframe.className = 'none';
+  iframe.setAttribute("src", "/app/imgs/bingo.png");
+  iframe.addEventListener('load', function () {
+    $timeout(function () {
+      iframe.removeEventListener('load', function () {
+      });
+      document.body.removeChild(iframe);
+    }, 0);
+  });
+  document.body.appendChild(iframe);
+};
+
 http.post('/notice/noticeDetails', {
   id: query.id
 }, function (data) {
+  changeTitle(data.title);
   title.innerHTML = data.title;
-  date.innerHTML = tools.dateFormat('YYYY-MM-DD HH:mm:ss',data.createTime);
+  date.innerHTML = tools.dateFormat('YYYY-MM-DD HH:mm:ss', data.createTime);
   context.innerHTML = data.text;
 });
