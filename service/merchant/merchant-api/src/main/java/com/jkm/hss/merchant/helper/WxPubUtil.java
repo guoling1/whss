c2f8742606247cbeabb80bdde95f4ee261dddf5d
xingliujie <java_xlj@163.com>
2017-06-06 18:13:43 +0800

    微信图片上传

--- service/merchant/merchant-api/src/main/java/com/jkm/hss/merchant/helper/WxPubUtil.java
+++ service/merchant/merchant-api/src/main/java/com/jkm/hss/merchant/helper/WxPubUtil.java
@@ -660,4 +660,35 @@ public class WxPubUtil {
 
     }
 
+    /**
+     *
+     * @param mediaId
+     * @return
+     */
+    public  static  InputStream getInputStream(String mediaId) {
+        Map<String, String> tokenAndTicket = getTokenAndJsapiTicket();
+        InputStream is = null;
+        String url = "http://file.api.weixin.qq.com/cgi-bin/media/get?access_token="
+                + tokenAndTicket.get("accessToken").toString() + "&media_id=" + mediaId;
+        log.debug("mediaId:"+ mediaId);
+        try {
+            URL urlGet = new URL(url);
+            HttpURLConnection http = (HttpURLConnection) urlGet
+                    .openConnection();
+            http.setRequestMethod("GET"); // 必须是get方式请求
+            http.setRequestProperty("Content-Type",
+                    "application/x-www-form-urlencoded");
+            http.setDoOutput(true);
+            http.setDoInput(true);
+            System.setProperty("sun.net.client.defaultConnectTimeout", "30000");// 连接超时30秒
+            System.setProperty("sun.net.client.defaultReadTimeout", "30000"); // 读取超时30秒
+            http.connect();
+            // 获取文件转化为byte流
+            is = http.getInputStream();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return is;
+
+    }
 }
