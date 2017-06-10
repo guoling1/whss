3ea20762b7936dbb6fac2a2f18b34314e67b2147
Administrator <706326182@qq.com>
2017-06-10 15:39:11 +0800

    代理商pc交易金额统计

--- service/bill/bill-service/src/main/java/com/jkm/hss/bill/service/impl/OrderServiceImpl.java
+++ service/bill/bill-service/src/main/java/com/jkm/hss/bill/service/impl/OrderServiceImpl.java
@@ -1498,18 +1498,10 @@ public class OrderServiceImpl implements OrderService {
 
         if("hss".equals(req.getAppId())){
             String list = this.orderDao.getAmountCount(req);
-            if ("".equals(list)&&list==null) {
-                list="0";
-                return list;
-            }
             return list;
         }
         if("hsy".equals(req.getAppId())){
             String list = this.orderDao.getAmountCount1(req);
-            if ("".equals(list)&&list==null) {
-                list="0";
-                return list;
-            }
             return list;
         }
 
@@ -1521,18 +1513,10 @@ public class OrderServiceImpl implements OrderService {
 
         if("hss".equals(req.getAppId())){
             String list = this.orderDao.getPoundageCount(req);
-            if ("".equals(list)&&list==null) {
-                list="0";
-                return list;
-            }
             return list;
         }
         if("hsy".equals(req.getAppId())){
             String list = this.orderDao.getPoundageCount1(req);
-            if ("".equals(list)&&list==null) {
-                list="0";
-                return list;
-            }
             return list;
         }
 
