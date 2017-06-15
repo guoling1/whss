95fa12a6efdcf75b491332337ce0e4904bda69bf
yulong.zhang <yulong.zhang@yulongzhangdeMacBook-Pro.local>
2017-06-15 17:08:22 +0800

    bug

--- service/bill/bill-service/src/main/java/com/jkm/hss/bill/service/impl/HSYOrderServiceImpl.java
+++ service/bill/bill-service/src/main/java/com/jkm/hss/bill/service/impl/HSYOrderServiceImpl.java
@@ -60,7 +60,7 @@ public class HSYOrderServiceImpl implements HSYOrderService {
             idStr=idStr.substring(idStr.length()-8);
         }
         hsyOrder.setOrdernumber(DateFormatUtil.format(new Date(),"yyyyMMdd")+idStr);
-        hsyOrder.setValidationcode(hsyOrder.getOrdernumber().substring(hsyOrder.getOrdernumber().length()-4));
+//        hsyOrder.setValidationcode(hsyOrder.getOrdernumber().substring(hsyOrder.getOrdernumber().length()-4));
         hsyOrderDao.update(hsyOrder);
     }
     private String getFixLenthString(int strLength) {
