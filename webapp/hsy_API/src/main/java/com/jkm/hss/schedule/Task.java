88696d44722cb4a45aeaeac72d29eeae01340f25
yulong.zhang <yulong.zhang@yulongzhangdeMacBook-Pro.local>
2017-07-12 15:09:35 +0800

    chronos

--- webapp/hsy_API/src/main/java/com/jkm/hss/schedule/Task.java
+++ webapp/hsy_API/src/main/java/com/jkm/hss/schedule/Task.java
@@ -27,36 +27,9 @@ import java.util.List;
 public class Task {
 
     @Autowired
-    @Qualifier("accountSettleAuditRecordService")
-    private AccountSettleAuditRecordService accountSettleAuditRecordService;
-
-    @Autowired
     private SendMqMsgService sendMqMsgService;
     @Autowired
     private HsyBalanceAccountEmailService hsyBalanceAccountEmailService;
-
-
-    /**
-     * 处理 T1 结算审核, 生成记录
-     *
-     * 2点0时0分
-     */
-//    @Scheduled(cron = "0 0 2 * * ?")
-    public void generateHsySettleAuditRecordTask() {
-        log.info("生成结算审核记录定时任务--start");
-//        this.accountSettleAuditRecordService.generateHsySettleAuditRecordTask();
-        log.info("生成结算审核记录定时任务--end");
-    }
-
-    /**
-     * 处理 T1 结算 自动结算
-     */
-//    @Scheduled(cron = "0 0 14 * * ?")
-    public void handleT1WithdrawTask() {
-        log.info("结算审核记录自动结算定时任务--start");
-//        this.accountSettleAuditRecordService.handleSettleAuditRecordTask();
-        log.info("结算审核记录自动结算定时任务--end");
-    }
 
     /**
      * 处理重发分润-每一小时一次
