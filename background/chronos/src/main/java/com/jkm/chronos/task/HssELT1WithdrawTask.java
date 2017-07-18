a1f9f6552351564f343c80246855569cf314e7c8
yulong.zhang <yulong.zhang@yulongzhangdeMacBook-Pro.local>
2017-07-18 10:41:26 +0800

    chronos

---
+++ background/chronos/src/main/java/com/jkm/chronos/task/HssELT1WithdrawTask.java
@@ -0,0 +1,36 @@
+package com.jkm.chronos.task;
+
+import com.jkm.hss.bill.service.OrderService;
+import com.jkm.hss.product.enums.EnumPayChannelSign;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+/**
+ * Created by yulong.zhang on 2017/7/18.
+ */
+@Slf4j
+@Component
+public class HssELT1WithdrawTask extends AbstractTask {
+    @Autowired
+    private OrderService orderService;
+
+    @Override
+    protected void run() {
+        final ArrayList<Integer> channelList = new ArrayList<>();
+        channelList.add(EnumPayChannelSign.EL_UNIONPAY.getId());
+        this.orderService.handleT1UnSettlePayOrder(channelList);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void handleJobExecutionException(final JobExecutionException jobExecutionException) throws JobExecutionException {
+        super.handleJobExecutionException(jobExecutionException);
+        log.error("重发分润任务执行失败:" + jobExecutionException.getMessage());
+    }
+}
