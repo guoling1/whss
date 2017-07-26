ab8558bb5b73c0c9bd7399fea46aa608cd9ef423
liucong <liucong@haodai.com>
2017-07-26 18:52:39 +0800

    会员卡支付宝相关 列表值状态

--- webapp/hsy_API/src/main/java/com/jkm/hss/controller/active/MembershipController.java
+++ webapp/hsy_API/src/main/java/com/jkm/hss/controller/active/MembershipController.java
@@ -402,12 +402,12 @@ public class MembershipController {
         Optional<MemberAccount> account=memberAccountService.getById(appPolicyMember.getAccountID());
 //        List<AppBizShop> appBizShopList=hsyMembershipService.findSuitShopByMCID(appPolicyMember.getMcid());
         appPolicyMember.setRemainingSum(account.get().getAvailable());
-        appPolicyMember.setRechargeTotalAmount(account.get().getRechargeTotalAmount());
+//        appPolicyMember.setRechargeTotalAmount(account.get().getRechargeTotalAmount());
         appPolicyMember.setConsumeTotalAmount(account.get().getConsumeTotalAmount());
-        if(appPolicyMember.getIsDeposited()==0) {
+//        if(appPolicyMember.getIsDeposited()==0) {
             BigDecimal totalAmount=hsyMembershipService.findConsumeOrderSum(appPolicyMember.getMcid(),appPolicyMember.getId());
             appPolicyMember.setConsumeTotalAmount(totalAmount);
-        }
+//        }
 
         DecimalFormat a=new DecimalFormat("0.0");
         String discountStr=a.format(appPolicyMember.getDiscount());
