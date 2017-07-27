4db5e1f345816faadceed74d5a30f15ad3105274
xingliujie <java_xlj@163.com>
2017-07-27 15:21:24 +0800

    开通D0提现

---
+++ service/hsyuser/user-api/src/main/java/com/jkm/hsy/user/entity/UserD0WithdrawRequest.java
@@ -0,0 +1,14 @@
+package com.jkm.hsy.user.entity;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * do提现请求参数
+ */
+@Data
+public class UserD0WithdrawRequest {
+    private Long userId;//用户编码
+    private Integer isOpenDo;//是否开通d0提现
+}
