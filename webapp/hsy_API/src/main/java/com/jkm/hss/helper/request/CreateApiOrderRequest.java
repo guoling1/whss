e02f651859dde5db4366421ce4257e1217c61531
xiang.yu <http://gitlab.jinkaimen.com>
2017-08-13 18:15:37 +0800

    api

--- webapp/hsy_API/src/main/java/com/jkm/hss/helper/request/CreateApiOrderRequest.java
+++ webapp/hsy_API/src/main/java/com/jkm/hss/helper/request/CreateApiOrderRequest.java
@@ -63,28 +63,28 @@ public class CreateApiOrderRequest {
 
     public void validate() {
         if (StringUtils.isEmpty(trxType) || !"WX_SCANCODE_JSAPI".equals(trxType) || !"Alipay_SCANCODE_JSAPI".equals(trxType)){
-            throw new JKMTradeServiceException(JkmApiErrorCode.COMMON_ERROR, "接口类型错误");
+            throw new JKMTradeServiceException(JkmApiErrorCode.COMMON_ERROR, "1接口类型错误");
         }
         if (StringUtils.isEmpty(merchantNo)){
-            throw new JKMTradeServiceException(JkmApiErrorCode.COMMON_ERROR, "null");
+            throw new JKMTradeServiceException(JkmApiErrorCode.COMMON_ERROR, "2商户号不能为空");
         }
         if (StringUtils.isEmpty(orderNum)){
-            throw new JKMTradeServiceException(JkmApiErrorCode.COMMON_ERROR, "商户订单号不能为空");
+            throw new JKMTradeServiceException(JkmApiErrorCode.COMMON_ERROR, "3商户订单号不能为空");
         }
         if (ValidateUtils.isNull(amount) || !ValidateUtils.isDouble(amount.toString())){
-            throw new JKMTradeServiceException(JkmApiErrorCode.COMMON_ERROR, "订单金额错误");
+            throw new JKMTradeServiceException(JkmApiErrorCode.COMMON_ERROR, "4订单金额错误");
         }
         if (StringUtils.isEmpty(goodsName)){
-            throw new JKMTradeServiceException(JkmApiErrorCode.COMMON_ERROR, "订单描述不能为空");
+            throw new JKMTradeServiceException(JkmApiErrorCode.COMMON_ERROR, "5订单描述不能为空");
         }
         if (StringUtils.isEmpty(callbackUrl)){
-            throw new JKMTradeServiceException(JkmApiErrorCode.COMMON_ERROR, "支付通知地址不能为空");
+            throw new JKMTradeServiceException(JkmApiErrorCode.COMMON_ERROR, "6支付通知地址不能为空");
         }
         if (StringUtils.isEmpty(orderIp)){
-            throw new JKMTradeServiceException(JkmApiErrorCode.COMMON_ERROR, "用户支付IP不能为空");
+            throw new JKMTradeServiceException(JkmApiErrorCode.COMMON_ERROR, "7用户支付IP不能为空");
         }
         if (StringUtils.isEmpty(sign)){
-            throw new JKMTradeServiceException(JkmApiErrorCode.COMMON_ERROR, "签名不能为空");
+            throw new JKMTradeServiceException(JkmApiErrorCode.COMMON_ERROR, "8签名不能为空");
         }
     }
 
